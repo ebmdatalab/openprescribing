5768140a365b77170b85b170538ea628ad31efee
Anna Powell-Smith <annapowellsmith@gmail.com>
2016-02-05 16:04:43 +0000

    Handle HSCIC's inconsistent file suffixes.
    HSCIC PDPI files are sometimes suffixed with .csv instead of .CSV.
    
    Don't skip these files.

--- openprescribing/frontend/management/commands/convert_hscic_prescribing.py
+++ openprescribing/frontend/management/commands/convert_hscic_prescribing.py
@@ -24,7 +24,7 @@ class Command(BaseCommand):
         if options['filename']:
             filenames = [options['filename']]
         else:
-            filenames = glob.glob('./data/raw_data/T*PDPI+BNFT.CSV')
+            filenames = glob.glob('./data/raw_data/T*PDPI+BNFT.*')
 
         for f in filenames:
             if self.IS_VERBOSE:
