32c9fe9c6e35859380f2088f25986285c7dc4b64
Anna Powell-Smith <annapowellsmith@gmail.com>
2016-04-15 12:23:18 +0100

    Add API views for measures.
    Ensure that our API is returning the fields we expect, including for
    practices with no prescribing of either numerator or denominator.
    These practices should return a null `calc_value`.

--- openprescribing/api/views_measures.py
+++ openprescribing/api/views_measures.py
@@ -3,31 +3,72 @@ from rest_framework.response import Response
 import view_utils as utils
 from frontend.models import Measure, MeasureGlobal, MeasureValue
 
-
 @api_view(['GET'])
 def measure_global(request, format=None):
     measure = request.query_params.get('measure', None)
     if measure:
         m = Measure.objects.get(id=measure)
-        mdata = {
-            'name': m.name,
-            'title': m.title
-        }
     else:
-        mdata = {}
+        m = Measure.objects.all()
 
-    query = 'SELECT month AS date, numerator, denominator, measure_id, '
-    query += 'calc_value, practice_10th, practice_25th, practice_50th, '
-    query += 'practice_75th, practice_90th, ccg_10th, '
-    query += 'ccg_25th, ccg_50th, ccg_75th, ccg_90th '
-    query += "FROM frontend_measureglobal "
+    query = 'SELECT mg.month AS date, mg.numerator, mg.denominator, mg.measure_id, '
+    query += 'mg.calc_value, practice_10th, mg.practice_25th, mg.practice_50th, '
+    query += 'mg.practice_75th, mg.practice_90th, mg.ccg_10th, '
+    query += 'mg.ccg_25th, mg.ccg_50th, mg.ccg_75th, mg.ccg_90th, '
+    query += 'mg.cost_saving_10th, mg.cost_saving_25th, mg.cost_saving_50th, '
+    query += 'mg.cost_saving_75th, mg.cost_saving_90th, '
+    query += 'ms.name, ms.title, ms.description, ms.numerator_description, '
+    query += 'ms.denominator_description, ms.denominator_short, ms.numerator_short, '
+    query += 'ms.url, ms.is_cost_based '
+    query += "FROM frontend_measureglobal mg "
+    query += "JOIN frontend_measure ms ON mg.measure_id=ms.id "
     if measure:
-        query += "WHERE measure_id=%s "
-    query += "ORDER BY measure_id, date"
+        query += "WHERE mg.measure_id=%s "
+    query += "ORDER BY mg.measure_id, mg.month"
 
+    data = utils.execute_query(query, [[measure]])
+    rolled = {}
+    for d in data:
+        id = d['measure_id']
+        d_copy = {
+            'date': d['date'],
+            'numerator': d['numerator'],
+            'denominator': d['denominator'],
+            'calc_value': d['calc_value'],
+            'practice_10th': d['practice_10th'],
+            'practice_25th': d['practice_25th'],
+            'practice_50th': d['practice_50th'],
+            'practice_75th': d['practice_75th'],
+            'practice_90th': d['practice_90th'],
+            'ccg_10th': d['ccg_10th'],
+            'ccg_25th': d['ccg_25th'],
+            'ccg_50th': d['ccg_50th'],
+            'ccg_75th': d['ccg_75th'],
+            'ccg_90th': d['ccg_90th'],
+            'cost_saving_10th': d['cost_saving_10th'],
+            'cost_saving_25th': d['cost_saving_25th'],
+            'cost_saving_50th': d['cost_saving_50th'],
+            'cost_saving_75th': d['cost_saving_75th'],
+            'cost_saving_90th': d['cost_saving_90th']
+        }
+        if id in rolled:
+            rolled[id]['data'].append(d_copy)
+        else:
+            rolled[id] = {
+                'id': id,
+                'name': d['name'],
+                'title': d['title'],
+                'description': d['description'],
+                'numerator_description': d['numerator_description'],
+                'denominator_description': d['denominator_description'],
+                'numerator_short': d['numerator_short'],
+                'denominator_short': d['denominator_short'],
+                'url': d['url'],
+                'is_cost_based': d['is_cost_based'],
+                'data': [d_copy]
+            }
     d = {
-        'measure': mdata,
-        'data': utils.execute_query(query, [[measure]])
+        'measures': [rolled[k] for k in rolled]
     }
     return Response(d)
 
@@ -54,34 +95,69 @@ def measure_by_practice(request, format=None):
     measure = request.query_params.get('measure', None)
     orgs = utils.param_to_list(request.query_params.get('org', []))
 
-    query = 'SELECT pr.month AS date, pr.numerator, pr.denominator, '
-    query += 'pr.calc_value, pr.percentile, pr.cost_saving, '
-    query += 'pr.practice_id, pc.name as name, measure_id '
-    query += "FROM frontend_measurevalue pr "
-    query += "JOIN frontend_practice pc ON pr.practice_id=pc.code "
+    query = 'SELECT mv.month AS date, mv.numerator, mv.denominator, '
+    query += 'mv.calc_value, mv.percentile, mv.cost_saving_10th, '
+    query += 'mv.cost_saving_25th, mv.cost_saving_50th, '
+    query += 'mv.cost_saving_75th, mv.cost_saving_90th, '
+    query += 'mv.practice_id, pc.name as practice_name, measure_id, '
+    query += 'ms.name, ms.title, ms.description, ms.numerator_description, '
+    query += 'ms.denominator_description, ms.denominator_short, ms.numerator_short, '
+    query += 'ms.url, ms.is_cost_based '
+    query += "FROM frontend_measurevalue mv "
+    query += "JOIN frontend_practice pc ON mv.practice_id=pc.code "
+    query += "JOIN frontend_measure ms ON mv.measure_id=ms.id "
     query += "WHERE "
     for i, org in enumerate(orgs):
         if len(org) == 3:
-            query += "pr.pct_id=%s "
+            query += "mv.pct_id=%s "
         else:
-            query += "pr.practice_id=%s "
+            query += "mv.practice_id=%s "
         if (i != len(orgs)-1):
             query += ' OR '
     if measure:
-        query += "AND pr.measure_id=%s "
-    query += "ORDER BY pr.practice_id, measure_id, date"
+        query += "AND mv.measure_id=%s "
+    query += "ORDER BY mv.practice_id, measure_id, date"
 
     if measure:
-        m = Measure.objects.get(id=measure)
-        d = {
-            'measure': {
-                'name': m.name,
-                'title': m.title
-            },
-            'data': utils.execute_query(query, [orgs, [measure]])
-        }
+        data = utils.execute_query(query, [orgs, [measure]])
     else:
-        d = {
-            'data': utils.execute_query(query, [orgs])
+        data = utils.execute_query(query, [orgs])
+
+    rolled = {}
+    for d in data:
+        id = d['measure_id']
+        d_copy = {
+            'date': d['date'],
+            'numerator': d['numerator'],
+            'denominator': d['denominator'],
+            'calc_value': d['calc_value'],
+            'percentile': d['percentile'],
+            'cost_saving_10th': d['cost_saving_10th'],
+            'cost_saving_25th': d['cost_saving_25th'],
+            'cost_saving_50th': d['cost_saving_50th'],
+            'cost_saving_75th': d['cost_saving_75th'],
+            'cost_saving_90th': d['cost_saving_90th'],
+            'practice_id': d['practice_id'],
+            'practice_name': d['practice_name']
         }
+        if id in rolled:
+            rolled[id]['data'].append(d_copy)
+        else:
+            rolled[id] = {
+                'id': id,
+                'name': d['name'],
+                'title': d['title'],
+                'description': d['description'],
+                'numerator_description': d['numerator_description'],
+                'denominator_description': d['denominator_description'],
+                'numerator_short': d['numerator_short'],
+                'denominator_short': d['denominator_short'],
+                'url': d['url'],
+                'is_cost_based': d['is_cost_based'],
+                'data': [d_copy]
+            }
+
+    d = {
+        'measures': [rolled[k] for k in rolled]
+    }
     return Response(d)
--- openprescribing/frontend/tests/test_api_measures.py
+++ openprescribing/frontend/tests/test_api_measures.py
@@ -5,118 +5,160 @@ import unittest
 from django.core import management
 from django.test import TestCase
 from common import utils
-from frontend.models import SHA, PCT, Chemical, Practice, Prescription
+from frontend.models import SHA, PCT, Chemical, Practice
 
 
 def setUpModule():
-    sha = SHA.objects.create(code='Q51')
-    pct = PCT.objects.create(code='02Q')
-    p1 = Practice.objects.create(code='C84001',
-                                 name='LARWOOD SURGERY', setting=4)
-    p2 = Practice.objects.create(code='C84024',
-                                 name='NEWGATE MEDICAL GROUP', setting=4)
-    Practice.objects.create(code='Y00581',
-                            name='BASSETLAW DRUG & ALCOHOL SERVICE',
-                            setting=4)
+        SHA.objects.create(code='Q51')
+        PCT.objects.create(code='02Q')
+        Chemical.objects.create(bnf_code='0212000AA',
+                                    chem_name='Rosuvastatin Calcium')
+        Chemical.objects.create(bnf_code='0212000B0',
+                                    chem_name='Atorvastatin')
+        Practice.objects.create(code='C84001',
+                                name='LARWOOD SURGERY', setting=4)
+        Practice.objects.create(code='C84024',
+                                name='NEWGATE MEDICAL GROUP', setting=4)
+        Practice.objects.create(code='B82005',
+                                name='PRIORY MEDICAL GROUP', setting=4,
+                                open_date='2015-01-01')
+        Practice.objects.create(code='B82010',
+                                name='RIPON SPA SURGERY', setting=4)
+        Practice.objects.create(code='A85017',
+                                name='BEWICK ROAD SURGERY', setting=4)
+        Practice.objects.create(code='A86030',
+                                name='BETTS AVENUE MEDICAL GROUP', setting=4)
+        # Ensure we only include open practices in our calculations.
+        Practice.objects.create(code='B82008',
+                                name='NORTH HOUSE SURGERY', setting=4,
+                                open_date='2010-04-01',
+                                close_date='2012-01-01')
+        # Ensure we only include standard practices in our calculations.
+        Practice.objects.create(code='Y00581',
+                                name='BASSETLAW DRUG & ALCOHOL SERVICE',
+                                setting=1)
 
-    dosulepin = Chemical.objects.create(bnf_code='0403010J0',
-                                chem_name='Dosulepin Hydrochloride')
-    citalopram = Chemical.objects.create(bnf_code='0403030D0',
-                                chem_name='Citalopram Hydrobromide')
-    Prescription.objects.create(sha=sha, pct=pct, practice=p1, chemical=dosulepin,
-                                presentation_code='0403010J0AAAIAI',
-                                presentation_name='Dosulepin HCl_Tab 75mg',
-                                total_items=34,
-                                net_cost=63.79,
-                                actual_cost=59.53,
-                                quantity=1215,
-                                processing_date='2013-09-01',
-                                price_per_unit=0.048995884)
-    Prescription.objects.create(sha=sha, pct=pct, practice=p1, chemical=citalopram,
-                                presentation_code='0403030D0AAAAAA',
-                                presentation_name='Citalopram Hydrob_Tab 20mg',
-                                total_items=381,
-                                net_cost=422.43,
-                                actual_cost=395.91,
-                                quantity=11480,
-                                processing_date='2013-09-01',
-                                price_per_unit=0.03448693379790941)
-    Prescription.objects.create(sha=sha, pct=pct, practice=p2, chemical=dosulepin,
-                                presentation_code='0403010J0AAAIAI',
-                                presentation_name='Dosulepin HCl_Tab 75mg',
-                                total_items=34,
-                                net_cost=63.79,
-                                actual_cost=59.53,
-                                quantity=1215,
-                                processing_date='2013-09-01',
-                                price_per_unit=0.048995884)
-    Prescription.objects.create(sha=sha, pct=pct, practice=p2, chemical=citalopram,
-                                presentation_code='0403030E0AAACAC',
-                                presentation_name='Fluoxetine HCl_Oral Soln 20mg/5ml',
-                                total_items=2,
-                                net_cost=12.81,
-                                actual_cost=11.86,
-                                quantity=210,
-                                processing_date='2013-09-01',
-                                price_per_unit=0.05647619047619047)
-    args = []
-    opts = {
-        'month': '2013-09-01'
-    }
-    management.call_command('import_dosulepin', *args, **opts)
+        args = []
+        db_name = 'test_' + utils.get_env_setting('DB_NAME')
+        db_user = utils.get_env_setting('DB_USER')
+        db_pass = utils.get_env_setting('DB_PASS')
+        test_file = 'frontend/tests/fixtures/commands/'
+        test_file += 'T201509PDPI+BNFT_formatted.CSV'
+        new_opts = {
+            'db_name': db_name,
+            'db_user': db_user,
+            'db_pass': db_pass,
+            'filename': test_file
+        }
+        management.call_command('import_hscic_prescribing', *args, **new_opts)
+
+        month = '2015-09-01'
+        measure_id = 'rosuvastatin'
+        args = []
+        opts = {
+            'month': month,
+            'measure': measure_id
+        }
+        management.call_command('import_measures', *args, **opts)
 
 def tearDownModule():
     management.call_command('flush', verbosity=0, interactive=False)
-
 
 class TestAPIMeasureViews(TestCase):
 
     api_prefix = '/api/1.0'
 
     def test_api_measure_global(self):
-        url = '/api/1.0/measure/?measure=ktt8_dosulepin&format=json'
+        url = '/api/1.0/measure/?measure=rosuvastatin&format=json'
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
-        self.assertEqual(data['measure']['name'], 'KTT8 (Dosulepin)')
-        self.assertEqual(len(data['data']), 1)
-        d = data['data'][0]
-        self.assertEqual(d['numerator'], 68)
-        self.assertEqual(d['denominator'], 451)
-        self.assertEqual("%.4f" % d['calc_value'], '0.1508')
-        self.assertEqual("%.4f" % d['practice_10th'], '0.1682')
-        self.assertEqual("%.4f" % d['practice_25th'], '0.2976')
-        self.assertEqual("%.4f" % d['practice_50th'], '0.5132')
-        self.assertEqual("%.4f" % d['practice_75th'], '0.7288')
-        self.assertEqual("%.4f" % d['practice_90th'], '0.8582')
+        self.assertEqual(data['measures'][0]['name'], 'Rosuvastatin vs. Atorvastatin')
+        self.assertEqual(len(data['measures'][0]['data']), 1)
+        d = data['measures'][0]['data'][0]
+        self.assertEqual(d['numerator'], 82000)
+        self.assertEqual(d['denominator'], 143000)
+        self.assertEqual("%.4f" % d['calc_value'], '0.5734')
+        self.assertEqual("%.4f" % d['practice_10th'], '0.0364')
+        self.assertEqual("%.4f" % d['practice_25th'], '0.0909')
+        self.assertEqual("%.4f" % d['practice_50th'], '0.3333')
+        self.assertEqual("%.4f" % d['practice_75th'], '0.7000')
+        self.assertEqual("%.4f" % d['practice_90th'], '0.8800')
+        self.assertEqual("%.2f" % d['cost_saving_10th'], '69152.73')
+        self.assertEqual("%.2f" % d['cost_saving_25th'], '62181.82')
+        self.assertEqual("%.2f" % d['cost_saving_50th'], '33600.00')
+        self.assertEqual("%.2f" % d['cost_saving_75th'], '270.00')
+        self.assertEqual("%.2f" % d['cost_saving_90th'], '108.00')
 
     def test_api_all_measures_global(self):
         url = '/api/1.0/measure/?format=json'
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
-        self.assertEqual(len(data['data']), 1)
-        d = data['data'][0]
-        self.assertEqual(d['numerator'], 68)
-        self.assertEqual(d['denominator'], 451)
-        self.assertEqual("%.4f" % d['calc_value'], '0.1508')
-        self.assertEqual("%.4f" % d['practice_10th'], '0.1682')
-        self.assertEqual("%.4f" % d['practice_25th'], '0.2976')
-        self.assertEqual("%.4f" % d['practice_50th'], '0.5132')
-        self.assertEqual("%.4f" % d['practice_75th'], '0.7288')
-        self.assertEqual("%.4f" % d['practice_90th'], '0.8582')
+        self.assertEqual(len(data['measures'][0]['data']), 1)
+        d = data['measures'][0]['data'][0]
+        self.assertEqual(d['numerator'], 82000)
+        self.assertEqual(d['denominator'], 143000)
+        self.assertEqual("%.4f" % d['calc_value'], '0.5734')
 
     def test_api_measure_by_practice(self):
         url = '/api/1.0/measure_by_practice/'
-        url += '?org=C84001&measure=ktt8_dosulepin&format=json'
+        url += '?org=C84001&measure=rosuvastatin&format=json'
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
-        self.assertEqual(len(data['data']), 1)
-        self.assertEqual(data['data'][0]['numerator'], 34)
-        self.assertEqual(data['data'][0]['denominator'], 415)
-        calc_value = data['data'][0]['calc_value']
-        self.assertEqual("%.4f" % calc_value, '0.0819')
+        self.assertEqual(len(data['measures'][0]['data']), 1)
+        d = data['measures'][0]['data'][0]
+        self.assertEqual(d['numerator'], 1000)
+        self.assertEqual(d['denominator'], 11000)
+        self.assertEqual(d['percentile'], 25)
+        self.assertEqual("%.4f" % d['calc_value'], '0.0909')
+        self.assertEqual("%.2f" % d['cost_saving_10th'], '540.00')
+        self.assertEqual("%.2f" % d['cost_saving_25th'], '0.00')
+        self.assertEqual("%.2f" % d['cost_saving_50th'], '-2400.00')
+        self.assertEqual("%.2f" % d['cost_saving_75th'], '-6030.00')
+        self.assertEqual("%.2f" % d['cost_saving_90th'], '-7812.00')
+
+        # Practice with only Rosuva prescribing.
+        url = '/api/1.0/measure_by_practice/'
+        url += '?org=A85017&measure=rosuvastatin&format=json'
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        d = data['measures'][0]['data'][0]
+        self.assertEqual(d['numerator'], 1000)
+        self.assertEqual(d['denominator'], 1000)
+        self.assertEqual(d['percentile'], 100)
+        self.assertEqual(d['calc_value'], 1)
+        self.assertEqual("%.2f" % d['cost_saving_10th'], '867.27')
+
+        # Practice with only Atorva prescribing.
+        url = '/api/1.0/measure_by_practice/'
+        url += '?org=A86030&measure=rosuvastatin&format=json'
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        self.assertEqual(len(data['measures'][0]['data']), 1)
+        d = data['measures'][0]['data'][0]
+        self.assertEqual(d['numerator'], 0)
+        self.assertEqual(d['denominator'], 1000)
+        self.assertEqual(d['percentile'], 0)
+        self.assertEqual(d['calc_value'], 0)
+        self.assertEqual("%.2f" % d['cost_saving_10th'], '-32.73')
+
+        # Practice with no prescribing of either.
+        url = '/api/1.0/measure_by_practice/'
+        url += '?org=B82010&measure=rosuvastatin&format=json'
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        self.assertEqual(len(data['measures'][0]['data']), 1)
+        d = data['measures'][0]['data'][0]
+        self.assertEqual(d['numerator'], 0)
+        self.assertEqual(d['denominator'], 0)
+        self.assertEqual(d['percentile'], None)
+        self.assertEqual(d['calc_value'], None)
+        self.assertEqual(d['cost_saving_10th'], 0.0)
 
     def test_api_all_measures_by_practice(self):
         url = '/api/1.0/measure_by_practice/'
@@ -124,8 +166,9 @@ class TestAPIMeasureViews(TestCase):
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
-        self.assertEqual(len(data['data']), 1)
-        self.assertEqual(data['data'][0]['numerator'], 34)
-        self.assertEqual(data['data'][0]['denominator'], 415)
-        calc_value = data['data'][0]['calc_value']
-        self.assertEqual("%.4f" % calc_value, '0.0819')
+        self.assertEqual(len(data['measures'][0]['data']), 1)
+        d = data['measures'][0]['data'][0]
+        self.assertEqual(d['numerator'], 1000)
+        self.assertEqual(d['denominator'], 11000)
+        self.assertEqual(d['percentile'], 25)
+        self.assertEqual("%.4f" % d['calc_value'], '0.0909')
