d8200cebb0d487481f97488cab656953b25b5d3f
Seb Bacon <seb.bacon@gmail.com>
2016-05-17 13:58:06 +0100

    Support setting DB_HOST from environment
    This will allow us to run postgres in a different container, using
    `docker-compose`.

--- openprescribing/common/utils.py
+++ openprescribing/common/utils.py
@@ -2,10 +2,16 @@ from os import environ
 from django.core.exceptions import ImproperlyConfigured
 
 
-def get_env_setting(setting):
-    """ Get the environment setting or return exception """
+def get_env_setting(setting, default=None):
+    """ Get the environment setting.
+
+    Return the default, or raise an exception if none supplied
+    """
     try:
         return environ[setting]
     except KeyError:
-        error_msg = "Set the %s env variable" % setting
-        raise ImproperlyConfigured(error_msg)
+        if default:
+            return default
+        else:
+            error_msg = "Set the %s env variable" % setting
+            raise ImproperlyConfigured(error_msg)
