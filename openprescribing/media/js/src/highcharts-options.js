ca7d8fd3bd1ead915a4752469e55f6af3d5e8086
Seb Bacon <seb.bacon@gmail.com>
2016-05-20 10:12:09 +0100

    Fix some JS formatting/style issues
    We've agreed to use the [Google JavaScript Style
    Guide](https://google.github.io/styleguide/javascriptguide.xml).
    
    This commit is the output of `eslint --fix` when run using the [ESLint
    shareable config](https://github.com/google/eslint-config-google) for
    that Style Guide.
    
    The vast majority of fixes are whitespace changes.

--- openprescribing/media/js/src/highcharts-options.js
+++ openprescribing/media/js/src/highcharts-options.js
@@ -3,140 +3,140 @@ global.jQuery = require('jquery');
 global.$ = global.jQuery;
 
 var backgroundGradient = {
-    linearGradient: [0, 0, 500, 500],
-    stops: [
+  linearGradient: [0, 0, 500, 500],
+  stops: [
         [0, 'rgb(255, 255, 255)'],
         [1, 'rgb(240, 240, 255)']
-    ]
+  ]
 };
 Highcharts.theme = {
-    colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+  colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
              '#FF9655', '#FFF263', '#6AF9C4'],
-    chart: {
-        marginTop: 10,
-        marginRight: 30,
-        background: backgroundGradient,
-        shadow: {
-            'color': '#ccc',
-            'offsetX': 1,
-            'offsetY': 3,
-            'opacity': 0.6
-        },
-        borderRadius: 6,
-        renderTo: 'chart'
+  chart: {
+    marginTop: 10,
+    marginRight: 30,
+    background: backgroundGradient,
+    shadow: {
+      'color': '#ccc',
+      'offsetX': 1,
+      'offsetY': 3,
+      'opacity': 0.6
     },
-    title: {
-        style: {
-            color: '#000',
-            font: "bold 16px 'sofia-pro', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
-        }
-    },
-    subtitle: {
-        style: {
-            color: '#666666',
-            font: "bold 12px 'sofia-pro', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
-        }
-    },
-    legend: {
-        itemStyle: {
-            font: "9px 'sofia-pro', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-            color: 'black'
-        },
-        itemHoverStyle:{
-            color: 'gray'
-        }
+    borderRadius: 6,
+    renderTo: 'chart'
+  },
+  title: {
+    style: {
+      color: '#000',
+      font: "bold 16px 'sofia-pro', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
     }
+  },
+  subtitle: {
+    style: {
+      color: '#666666',
+      font: "bold 12px 'sofia-pro', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
+    }
+  },
+  legend: {
+    itemStyle: {
+      font: "9px 'sofia-pro', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+      color: 'black'
+    },
+    itemHoverStyle:{
+      color: 'gray'
+    }
+  }
 };
 Highcharts.setOptions(Highcharts.theme);
 Highcharts.setOptions({
-    lang: {
-        thousandsSep: ',',
-        numericSymbols: [ "k" , "m" , "G" , "T" , "P" , "E"]
-    }
+  lang: {
+    thousandsSep: ',',
+    numericSymbols: ["k", "m", "G", "T", "P", "E"]
+  }
 });
 
 var baseOptions = {
-    global: {
-      useUTC: false
-    },
-    chart: {
-        type: 'scatter',
-        zoomType: 'xy',
-        style: {
-            fontFamily: "'sofia-pro', 'Helvetica Neue', 'Arial', sans-serif"
-        }
-    },
-    credits: {
-        enabled: true,
-        position: {
-            align: 'center',
-            verticalAlign: 'top',
-            y: 96
-        },
-        style: {
-            color: '#ccc',
-            fontSize: '24px',
-            opacity: 0.2
-        },
-        text: "openprescribing.net"
-    },
-    title: {
-        text: null
-    },
-    xAxis: {
-        title: {
-            enabled: true,
-            text: 'Month'
-        },
-        type: 'datetime',
-        dateTimeLabelFormats: {
-            month: '%b \'%y'
-        },
-        gridLineWidth: 0,
-        minorGridLineWidth: 0
-    },
-    yAxis: {
-        min: 0,
-        gridLineWidth: 0,
-        tickWidth: 1,
-        lineWidth: 1
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'left',
-        verticalAlign: 'top',
-        x: 100,
-        y: 70,
-        floating: true,
-        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
-        borderWidth: 1
-    },
-    plotOptions: {
-        column: {
-            borderRadius: 2
-        },
-        scatter: {
-            allowPointSelect: true,
-            marker: {
-                states: {
-                    hover: {
-                        enabled: true,
-                        lineColor: 'rgba(240, 173, 78, 0.3)',
-                        lineWidth: 3,
-                        radius: 5,
-                        fillColor: 'rgba(240, 173, 78, 0.7)'
-                    },
-                    select: {
-                        enabled: true,
-                        lineColor: 'rgba(217, 83, 79, 0.3)',
-                        lineWidth: 3,
-                        radius: 7,
-                        fillColor: 'rgba(217, 83, 79, 0.7)'
-                    }
-                }
-            }
-        }
+  global: {
+    useUTC: false
+  },
+  chart: {
+    type: 'scatter',
+    zoomType: 'xy',
+    style: {
+      fontFamily: "'sofia-pro', 'Helvetica Neue', 'Arial', sans-serif"
     }
+  },
+  credits: {
+    enabled: true,
+    position: {
+      align: 'center',
+      verticalAlign: 'top',
+      y: 96
+    },
+    style: {
+      color: '#ccc',
+      fontSize: '24px',
+      opacity: 0.2
+    },
+    text: "openprescribing.net"
+  },
+  title: {
+    text: null
+  },
+  xAxis: {
+    title: {
+      enabled: true,
+      text: 'Month'
+    },
+    type: 'datetime',
+    dateTimeLabelFormats: {
+      month: '%b \'%y'
+    },
+    gridLineWidth: 0,
+    minorGridLineWidth: 0
+  },
+  yAxis: {
+    min: 0,
+    gridLineWidth: 0,
+    tickWidth: 1,
+    lineWidth: 1
+  },
+  legend: {
+    layout: 'vertical',
+    align: 'left',
+    verticalAlign: 'top',
+    x: 100,
+    y: 70,
+    floating: true,
+    backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+    borderWidth: 1
+  },
+  plotOptions: {
+    column: {
+      borderRadius: 2
+    },
+    scatter: {
+      allowPointSelect: true,
+      marker: {
+        states: {
+          hover: {
+            enabled: true,
+            lineColor: 'rgba(240, 173, 78, 0.3)',
+            lineWidth: 3,
+            radius: 5,
+            fillColor: 'rgba(240, 173, 78, 0.7)'
+          },
+          select: {
+            enabled: true,
+            lineColor: 'rgba(217, 83, 79, 0.3)',
+            lineWidth: 3,
+            radius: 7,
+            fillColor: 'rgba(217, 83, 79, 0.7)'
+          }
+        }
+      }
+    }
+  }
 };
 
 var barOptions = $.extend(true, {}, baseOptions);
@@ -146,15 +146,15 @@ barOptions.chart.renderTo = 'summarychart';
 barOptions.xAxis.type = 'category';
 barOptions.legend.enabled = false;
 barOptions.xAxis.labels = {
-    formatter: function() {
-        var str = this.value.substring(0, 17);
-        str += (this.value.length > 17) ? '...' : '';
-        return str;
-    },
-    rotation: -90,
-    style: {
-        fontSize: '8px'
-    }
+  formatter: function() {
+    var str = this.value.substring(0, 17);
+    str += (this.value.length > 17) ? '...' : '';
+    return str;
+  },
+  rotation: -90,
+  style: {
+    fontSize: '8px'
+  }
 };
 
 var lineOptions = $.extend(true, {}, baseOptions);
@@ -162,29 +162,29 @@ lineOptions.chart.type = 'line';
 lineOptions.legend.enabled = false;
 lineOptions.chart.marginTop = 60;
 lineOptions.plotOptions = {
-    series: {
-        marker: {
-            states: {
-                hover: {
-                    radiusPlus: 1,
-                    lineWidthPlus: 1
-                }
-            }
-        },
-        states: {
-            hover: {
-                lineWidthPlus: 2
-            }
+  series: {
+    marker: {
+      states: {
+        hover: {
+          radiusPlus: 1,
+          lineWidthPlus: 1
         }
+      }
+    },
+    states: {
+      hover: {
+        lineWidthPlus: 2
+      }
     }
+  }
 };
 
 var scatterOptions = $.extend(true, {}, baseOptions);
 var chartOptions = {
-    baseOptions: $.extend(true, {}, baseOptions),
-    barOptions: barOptions,
-    lineOptions: lineOptions,
-    scatterOptions: scatterOptions
+  baseOptions: $.extend(true, {}, baseOptions),
+  barOptions: barOptions,
+  lineOptions: lineOptions,
+  scatterOptions: scatterOptions
 };
 
 
@@ -203,37 +203,37 @@ dashOptions.legend.y = -40;
 dashOptions.legend.x = 90;
 dashOptions.legend.borderRadius = 3;
 dashOptions.yAxis.title = {
-    offset: 50,
-    style: {
-        fontSize: 9
-    }
+  offset: 50,
+  style: {
+    fontSize: 9
+  }
 };
 dashOptions.yAxis.tickLength = 5;
 dashOptions.yAxis.labels = {
-    x: -10,
-    y: 3,
-    style: {
-        fontSize: 9
-    }
+  x: -10,
+  y: 3,
+  style: {
+    fontSize: 9
+  }
 };
 dashOptions.xAxis.title.style = {
-        fontSize: 9
+  fontSize: 9
 };
 dashOptions.xAxis.tickLength = 5;
 dashOptions.xAxis.labels = {
-    y: 15,
-    align: 'center',
-    style: {
-        fontSize: 9
-    }
+  y: 15,
+  align: 'center',
+  style: {
+    fontSize: 9
+  }
 };
 
 var scatterOptions = $.extend(true, {}, baseOptions);
 var chartOptions = {
-    baseOptions: $.extend(true, {}, baseOptions),
-    barOptions: barOptions,
-    lineOptions: lineOptions,
-    dashOptions: dashOptions,
-    scatterOptions: scatterOptions
+  baseOptions: $.extend(true, {}, baseOptions),
+  barOptions: barOptions,
+  lineOptions: lineOptions,
+  dashOptions: dashOptions,
+  scatterOptions: scatterOptions
 };
 module.exports = chartOptions;
