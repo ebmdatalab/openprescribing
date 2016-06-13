cc67e9a9e1ebfee1489fef97a90a1f1a58d6c1ab
Seb Bacon <seb.bacon@gmail.com>
2016-06-13 09:18:08 +0100

    Drop materialized views individually.
    Useful for debugging database state problems

--- openprescribing/frontend/management/commands/drop_matviews.py
+++ openprescribing/frontend/management/commands/drop_matviews.py
@@ -20,13 +20,25 @@ class Command(BaseCommand):
         db_host = utils.get_env_setting('DB_HOST', '127.0.0.1')
         self.conn = psycopg2.connect(database=db_name, user=db_user,
                                      password=db_pass, host=db_host)
+        commands = []
         with self.conn.cursor() as c:
-            cmd = 'DROP MATERIALIZED VIEW vw__presentation_summary; '
-            cmd += 'DROP MATERIALIZED VIEW vw__presentation_summary_by_ccg; '
-            cmd += 'DROP MATERIALIZED VIEW vw__chemical_summary_by_ccg; '
-            cmd += 'DROP MATERIALIZED VIEW vw__chemical_summary_by_practice; '
-            cmd += 'DROP MATERIALIZED VIEW vw__practice_summary; '
-            cmd += 'DROP MATERIALIZED VIEW vw__ccgstatistics; '
-            c.execute(cmd)
-        self.conn.commit()
+            commands.append(
+                'DROP MATERIALIZED VIEW vw__presentation_summary; ')
+            commands.append(
+                'DROP MATERIALIZED VIEW vw__presentation_summary_by_ccg; ')
+            commands.append(
+                'DROP MATERIALIZED VIEW vw__chemical_summary_by_ccg; ')
+            commands.append(
+                'DROP MATERIALIZED VIEW vw__chemical_summary_by_practice; ')
+            commands.append(
+                'DROP MATERIALIZED VIEW vw__practice_summary; ')
+            commands.append(
+                'DROP MATERIALIZED VIEW vw__ccgstatistics; ')
+            for cmd in commands:
+                try:
+                    c.execute(cmd)
+                    self.conn.commit()
+                except psycopg2.ProgrammingError:
+                    print "ERROR running: %s" % cmd
+                    self.conn.rollback()
         self.conn.close()
