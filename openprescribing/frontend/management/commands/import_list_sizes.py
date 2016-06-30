47a7dd290df8265c83ebcd50b9d9b1f7fb6671a2
Seb Bacon <seb.bacon@gmail.com>
2016-06-30 13:40:12 +0100

    Make importing list sizes idempotent
    Running the `import_list_sizes` management command with a path to a
    filename will cause the data in that file to be applied for all
    prescribing data that has been imported for the quarter to which that
    file pertains.
    
    There is also a check to ensure that the latest prescribing data is not
    newer than the list sizes data, as a gap in applied list size data
    should be considered an error condition.
    
    Note that the date logic continues to rely on the user correctly naming
    a file path: rather than the filename mattering, it's now the correct
    use of the `%Y_%m/`-containing-folder-convention in the source data that
    is used to determine the last date to which the list size data is valid.

--- openprescribing/frontend/management/commands/import_list_sizes.py
+++ openprescribing/frontend/management/commands/import_list_sizes.py
@@ -1,10 +1,32 @@
 import csv
-import glob
+import sys
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
-from frontend.models import Practice, PracticeStatistics, PCT
+from frontend.models import Practice, PracticeStatistics, PCT, ImportLog
 
+"""List size data comes in the form of a file that covers a particular
+quarter. The quarter for which it is valid is not apparent from the
+data or from the file name; only from the page on the NHSBSA website
+from which it is downloaded.
+
+Therefore, we encode this knowledge in the folder structure used in
+the openprescribing-data repository layout, e.g.:
+
+    data/patient_list_size/2016_03/Patient_List_Size_2016_05_12.csv
+
+This indicates that the file covers the quarter to March 2016. The
+dates in the filename are not relevant to the import process.
+
+We maintain a log of when the prescribing and patient list size
+datasets were last imported. Using this log, we can work out the
+months for which patient list size data is required.
+
+We insist on importing patient list data sequentially, to ensure there
+are no gaps in the data.
+
+"""
 
 class Command(BaseCommand):
     args = ''
@@ -17,22 +39,38 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--filename')
+        parser.add_argument('filenames', nargs='*')
 
     def handle(self, *args, **options):
         self.IS_VERBOSE = False
+        filename_usage = (
+            "You must either specify a filename or pass multiple filenames "
+            "as positional arguments, e.g. `python manage.py "
+            "import_list_sizes ../openprescribing-data/data/"
+            "patient_list_size/*/*csv`, or `python manage.py import_list_sizes"
+            " --filename ../openprescribing-data/data/2016_02/"
+            "Patient_List_Size_2016-02-01.csv`, but not both"
+        )
         if options['verbosity'] > 1:
             self.IS_VERBOSE = True
+        if options['filename'] and options['filenames']:
+            raise CommandError(filename_usage)
         if options['filename']:
             filenames = [options['filename']]
+        elif options['filenames']:
+            filenames = options['filenames']
         else:
-            filenames = glob.glob('data/list_sizes/Patient_List_Size*.csv')
-        for f in filenames:
+            raise CommandError(filename_usage)
+        filenames = sorted(filenames)
+        months_without_list_data = \
+            self.months_with_prescribing_data_but_no_list_data(self.IS_VERBOSE)
+        for filename in filenames:
             if self.IS_VERBOSE:
-                print f
-            entries = csv.DictReader(open('%s' % f, 'rU'))
+                print "Processing %s" % filename
+            entries = csv.DictReader(open('%s' % filename, 'rU'))
             if 'Month' in entries.fieldnames:
-                # This is the BSA's multi-year data: we don't need to
-                # derive the months from the filename.
+                # This is the BSA's multi-year data from an FOI request:
+                # we don't need to derive the months from the filename.
                 # What we do need to do is fill in the missing months.
                 for row in entries:
                     m = row['Month'].split('/')
@@ -44,19 +82,74 @@ class Command(BaseCommand):
                     n = [m[0], str(int(m[1]) + 2), m[2]]
                     self.process_row(row, '20' + '-'.join(n))
             else:
-                months = self.get_months_from_filename(f)
-                for row in entries:
-                    for m in months:
-                        self.process_row(row, m)
+                importfile_quarter_end = datetime.strptime(
+                    filename.split("/")[-2] + "_01", "%Y_%m_%d")
+                importfile_quarter_start = (importfile_quarter_end +
+                                            relativedelta(months=-3))
+                list_size_gap = [
+                    required for required in months_without_list_data
+                    if datetime.strptime(required, "%Y-%m-%d") <
+                    importfile_quarter_start
+                ]
+                if list_size_gap:
+                    msg = ("The supplied patient list size data file %s "
+                           "covers the period %s to %s. To avoid gaps in the "
+                           "data, you must import patient list size data in "
+                           "date order; but the following months of "
+                           "prescribing data are missing patient list sizes: "
+                           "%s. Run the command again supplying list size "
+                           "data current for these months.")
+                    raise CommandError(msg % (filename,
+                                              importfile_quarter_start,
+                                              importfile_quarter_end,
+                                              ', '.join(list_size_gap)))
+                if self.IS_VERBOSE:
+                    print ("Applying list size data for months %s to %s" %
+                           (importfile_quarter_start, importfile_quarter_end))
+                for i, row in enumerate(entries):
+                    if self.IS_VERBOSE and i % 100 == 0:
+                        sys.stdout.write(".")
+                        sys.stdout.flush()
+                    for m in months_without_list_data:
+                        if m > importfile_quarter_end.strftime('%Y-%m-%d'):
+                            break
+                        else:
+                            self.process_row(row, m)
+                if self.IS_VERBOSE:
+                    print
+                    print ("Timestamping patient_list_log with date %s" %
+                           importfile_quarter_end)
+                ImportLog.objects.create(
+                    current_at=importfile_quarter_end,
+                    filename=filename,
+                    category='patient_list_size'
+                )
+                months_without_list_data = \
+                    self.months_with_prescribing_data_but_no_list_data(
+                        self.IS_VERBOSE)
+                if not months_without_list_data:
+                    break
 
-    def get_months_from_filename(self, filename):
-        f = filename.replace('.csv', '').split('_')
-        year = f[-2]
-        first_month = int(f[-1].split('-')[0])
+    def months_with_prescribing_data_but_no_list_data(self, verbose=False):
+        prescribing_date = ImportLog.objects.latest_in_category(
+            'prescribing').current_at
+        list_size_date = ImportLog.objects.latest_in_category(
+            'patient_list_size').current_at
+        rd = relativedelta(prescribing_date, list_size_date)
+        total_months = rd.years * 12 + rd.months
         months = []
-        for i in range(0, 3):
-            month = first_month + i
-            months.append('%s-%s-01' % (year, month))
+        if total_months:
+            for i in range(0, total_months):
+                list_size_date = list_size_date + \
+                    relativedelta(months=1)
+                months.append(list_size_date.strftime("%Y-%m-01"))
+        if verbose:
+            if months:
+                print ('Prescribing data lacking patient list data: '
+                       '%s to %s' % (months[0],
+                                     months[-1]))
+            else:
+                print 'All prescribing data and patient list data up to date'
         return months
 
     def process_row(self, row, month):
--- openprescribing/frontend/tests/commands/test_import_list_sizes.py
+++ openprescribing/frontend/tests/commands/test_import_list_sizes.py
@@ -1,7 +1,10 @@
 from django.core.management import call_command
+from django.core.management.base import CommandError
 from django.test import TestCase
-from frontend.models import Practice, PracticeStatistics
+from frontend.models import Practice, PracticeStatistics, ImportLog
+from frontend.management.commands.import_list_sizes import Command
 
+PRESCRIBING_DATE = '2040-03-01'
 
 def setUpModule():
     Practice.objects.create(code='N84014',
@@ -10,6 +13,14 @@ def setUpModule():
                             name='BARLOW MEDICAL CENTRE')
     Practice.objects.create(code='Y02229',
                             name='ADDACTION NUNEATON')
+    ImportLog.objects.create(
+        current_at='2039-12-01',
+        category='patient_list_size'
+    )
+    ImportLog.objects.create(
+        current_at=PRESCRIBING_DATE,
+        category='prescribing'
+    )
 
 
 def tearDownModule():
@@ -17,22 +28,48 @@ def tearDownModule():
 
 
 class CommandsTestCase(TestCase):
+    def test_error_for_missing_list_data(self):
+        """Supplied patient list size data must always cover any existing gaps
+        in previously-imported list size data.
+
+        """
+        args = []
+        fname = 'frontend/tests/fixtures/commands/'
+        fname += 'patient_list_size/2040_05/Patient_List_Size_2013_10-12.csv'
+        opts = {
+            'filename': fname,
+            'verbosity': 2
+        }
+        # Now check that we get an error (because there is now a gap)
+        with self.assertRaises(CommandError):
+            call_command('import_list_sizes', *args, **opts)
+
+    def test_import_bsa_multiple_list_size(self):
+        fname = ("frontend/tests/fixtures/commands/patient_list_size/"
+                 "%s/Patient_List_Size_2013_10-12.csv")
+        args = [fname % '2040_02', fname % '2040_05']
+        opts = {}
+        call_command('import_list_sizes', *args, **opts)
+
+        list_sizes = PracticeStatistics.objects.all()
+        self.assertEqual(len(list_sizes), 9)
+        self.assertEqual(str(list_sizes[8].date), '2040-03-01')
 
     def test_import_bsa_list_size_quarterly(self):
 
         args = []
         fname = 'frontend/tests/fixtures/commands/'
-        fname += 'Patient_List_Size_2013_10-12.csv'
+        fname += 'patient_list_size/2040_02/Patient_List_Size_2013_10-12.csv'
         opts = {
             'filename': fname
         }
         call_command('import_list_sizes', *args, **opts)
-
+        last_list_size_date = '2040-02-01'
         list_sizes = PracticeStatistics.objects.all()
-        self.assertEqual(len(list_sizes), 9)
+        self.assertEqual(len(list_sizes), 6)
 
         p = PracticeStatistics.objects.get(practice_id='N84014',
-                                           date='2013-10-01')
+                                           date=last_list_size_date)
         self.assertEqual(p.total_list_size, 2932)
         self.assertEqual(p.astro_pu_cost, 12358.6840999993)
         self.assertEqual(p.astro_pu_items, 45377.5747635734)
@@ -48,7 +85,7 @@ class CommandsTestCase(TestCase):
 
         # Test that our script creates all months in the quarter.
         p = PracticeStatistics.objects.get(practice_id='N84014',
-                                           date='2013-11-01')
+                                           date=last_list_size_date)
         self.assertEqual(p.total_list_size, 2932)
         self.assertEqual(p.astro_pu_cost, 12358.6840999993)
         self.assertEqual(p.astro_pu_items, 45377.5747635734)
@@ -60,7 +97,7 @@ class CommandsTestCase(TestCase):
                          '66516.700')
 
         p = PracticeStatistics.objects.get(practice_id='N84014',
-                                           date='2013-12-01')
+                                           date=last_list_size_date)
         self.assertEqual(p.total_list_size, 2932)
         self.assertEqual(p.astro_pu_cost, 12358.6840999993)
         self.assertEqual(p.astro_pu_items, 45377.5747635734)
@@ -72,7 +109,7 @@ class CommandsTestCase(TestCase):
                          '66516.700')
 
         p = PracticeStatistics.objects.get(practice_id='P84034',
-                                           date='2013-12-01')
+                                           date=last_list_size_date)
         self.assertEqual(p.total_list_size, 13439)
         self.assertEqual(p.astro_pu_cost, 41303.5675254791)
         self.assertEqual(p.astro_pu_items, 144028.821414122)
@@ -84,7 +121,7 @@ class CommandsTestCase(TestCase):
                          '295093.300')
 
         p = PracticeStatistics.objects.get(practice_id='Y02229',
-                                           date='2013-12-01')
+                                           date=last_list_size_date)
         self.assertEqual(p.total_list_size, 0)
         self.assertEqual(p.astro_pu_cost, 0)
         self.assertEqual(p.astro_pu_items, 0)
@@ -121,3 +158,48 @@ class CommandsTestCase(TestCase):
         self.assertEqual(p.total_list_size, 12842)
         self.assertEqual(p.astro_pu_cost, 39098.6573715275)
         self.assertEqual(p.astro_pu_items, 136780.949819498)
+
+
+class MissingMonthsTestCase(TestCase):
+    """In order to track which months of patient list data are required,
+    we store last-imported dates for both lists. These tests check
+    that the missing months returned by the method in question are correct.
+
+    """
+    def test_imported_list_sizes_are_up_to_date_with_prescribing_data(self):
+        ImportLog.objects.create(
+            current_at='2050-01-01',
+            category='patient_list_size'
+        )
+        ImportLog.objects.create(
+            current_at='2050-01-01',
+            category='prescribing'
+        )
+        self.assertEqual(Command().months_with_prescribing_data_but_no_list_data(), [])
+
+    def test_imported_list_sizes_are_behind_prescribing_data(self):
+        ImportLog.objects.create(
+            current_at='2050-01-01',
+            category='patient_list_size'
+        )
+        ImportLog.objects.create(
+            current_at='2050-03-01',
+            category='prescribing'
+        )
+
+        self.assertEqual(
+            Command().months_with_prescribing_data_but_no_list_data(),
+            ['2050-02-01', '2050-03-01'])
+
+    def test_imported_list_sizes_are_ahead_of_prescribing_data(self):
+        ImportLog.objects.create(
+            current_at='2050-04-01',
+            category='patient_list_size'
+        )
+        ImportLog.objects.create(
+            current_at='2050-03-01',
+            category='prescribing'
+        )
+        self.assertEqual(
+            Command().months_with_prescribing_data_but_no_list_data(),
+            [])
