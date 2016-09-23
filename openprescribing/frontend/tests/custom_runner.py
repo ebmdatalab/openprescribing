e2ef85dc8775e6df6dae9fcccbd4eadbe6de6a18
Seb Bacon <seb.bacon@gmail.com>
2016-09-23 08:36:09 +0100

    Allow NPM build to be skipped in tests

--- openprescribing/frontend/tests/custom_runner.py
+++ openprescribing/frontend/tests/custom_runner.py
@@ -13,9 +13,10 @@ class AssetBuildingTestRunner(DiscoverRunner):
         settings.DJANGO_SETTINGS_MODULE = 'frontend.settings.test'
         # Before we load any func tests, ensure we've got assets built
         npm_cmd = "mkdir -p ../../static/js && npm run build"
-        subprocess.check_call(
-            npm_cmd, shell=True, cwd=settings.SITE_ROOT + '/media/js')
-
+        if 'SKIP_NPM_BUILD' not in os.environ:
+            subprocess.check_call(
+                npm_cmd, shell=True, cwd=settings.SITE_ROOT + '/media/js')
+        os.environ['DB_NAME'] = 'test_openprescribing'
         if not os.environ.get('BROWSER'):
             # Default test environment for Saucelabs
             os.environ['BROWSER'] = 'firefox:47.0:Windows 10'
