c4e455710d3f6e75131dd5e6e1c118949a7adcf9
Seb Bacon <seb.bacon@gmail.com>
2016-10-27 18:00:31 +0100

    Revert back to one decile for interesting changes

--- openprescribing/frontend/views/bookmark_utils.py
+++ openprescribing/frontend/views/bookmark_utils.py
@@ -82,7 +82,7 @@ def remove_jagged_logit(measurevalues):
 class InterestingMeasureFinder(object):
     def __init__(self, practice=None, pct=None,
                  interesting_saving=1000,
-                 interesting_percentile_change=20):
+                 interesting_percentile_change=10):
         assert practice or pct
         self.practice = practice
         self.pct = pct
