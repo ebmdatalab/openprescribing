3d11e0998b767eff966d486d5d62ebfc213b6e7d
Seb Bacon <seb.bacon@gmail.com>
2016-10-31 14:47:23 +0000

    Stub event handlers for logging email actions

--- openprescribing/frontend/signals/handlers.py
+++ openprescribing/frontend/signals/handlers.py
@@ -1,3 +1,4 @@
+from anymail.signals import tracking
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -17,3 +18,31 @@ def handle_user_save(sender, instance, created, **kwargs):
 def handle_user_logged_in(sender, request, user, **kwargs):
     user.searchbookmark_set.update(approved=True)
     user.orgbookmark_set.update(approved=True)
+
+
+@receiver(tracking)
+def handle_open(sender, event, esp_name, **kwargs):
+    if event.event_type == 'opened':
+        print("Message %s to %s bounced" % (
+              event.message_id, event.recipient))
+
+
+@receiver(tracking)
+def handle_click(sender, event, esp_name, **kwargs):
+    if event.event_type == 'clicked':
+        print("Recipient %s clicked url %s" % (
+              event.recipient, event.click_url))
+
+
+@receiver(tracking)
+def handle_deliver(sender, event, esp_name, **kwargs):
+    if event.event_type == 'delivered':
+        print("Recipient %s clicked url %s" % (
+              event.recipient, event.click_url))
+
+
+@receiver(tracking)
+def handle_unsubscribe(sender, event, esp_name, **kwargs):
+    if event.event_type == 'unsubscribed':
+        print("Recipient %s clicked url %s" % (
+              event.recipient, event.click_url))
