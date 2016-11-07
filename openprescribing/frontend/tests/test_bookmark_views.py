e2a94affb36b0a2c46c21ddf8878ba1c7692d32e
Seb Bacon <seb.bacon@gmail.com>
2016-11-07 09:57:00 +0000

    Fix broken tests relating to bookmark counts

--- openprescribing/frontend/tests/test_bookmark_views.py
+++ openprescribing/frontend/tests/test_bookmark_views.py
@@ -15,7 +15,7 @@ class TestBookmarkViews(TransactionTestCase):
     fixtures = ['bookmark_alerts']
 
     def _get_bookmark_url_for_user(self):
-        key = User.objects.last().profile.key
+        key = User.objects.get(username='bookmarks-user').profile.key
         return reverse('bookmark-login', kwargs={'key': key})
 
     def test_list_bookmarks_not_logged_in(self):
@@ -26,14 +26,14 @@ class TestBookmarkViews(TransactionTestCase):
         url = self._get_bookmark_url_for_user()
         response = self.client.get(url, follow=True)
         self.assertContains(
-            response, "You are currently subscribed to 2 monthly alerts")
+            response, "You are currently subscribed to 3 monthly alerts")
 
     def test_unsubscribe_one_by_one(self):
         # First, log in
         url = self._get_bookmark_url_for_user()
         self.client.get(url, follow=True)
         # There should be 2 bookmarks to get rid of
-        for _ in range(2):
+        for _ in range(3):
             data = {'org_bookmarks': [OrgBookmark.objects.first().pk]}
             response = self.client.post(
                 reverse('bookmark-list'), data, follow=True)
@@ -51,8 +51,10 @@ class TestBookmarkViews(TransactionTestCase):
             reverse('bookmark-list'), data, follow=True)
         self.assertContains(
             response,
-            "Unsubscribed from 2 alerts")
-        self.assertEqual(OrgBookmark.objects.count(), 0)
+            "Unsubscribed from 3 alerts")
+        # There's one org bookmark which is unapproved, so they don't
+        # see it in their list, and it doesn't get deleted.
+        self.assertEqual(OrgBookmark.objects.count(), 1)
 
     @patch('frontend.views.bookmark_utils.InterestingMeasureFinder')
     @patch('frontend.views.bookmark_utils.subprocess')
@@ -68,10 +70,10 @@ class TestBookmarkViews(TransactionTestCase):
         tmpfile.return_value.__enter__.return_value.name = test_img_path
         url = reverse('preview-ccg-bookmark',
                       kwargs={'code': PCT.objects.first().pk})
-        self.client.force_login(User.objects.first())
+        self.client.force_login(User.objects.get(username='admin-user'))
         response = self.client.get(url)
         self.assertContains(
-            response, "This CCG slipped by 20")
+            response, "this CCG")
         with open(test_img_path, 'rb') as expected:
             self.assertContains(
                 response, base64.b64encode(expected.read()))
@@ -83,7 +85,7 @@ class TestBookmarkViews(TransactionTestCase):
         finder.return_value.context_for_org_email.return_value = context
         url = reverse('preview-practice-bookmark',
                       kwargs={'code': Practice.objects.first().pk})
-        self.client.force_login(User.objects.first())
+        self.client.force_login(User.objects.get(username='admin-user'))
         response = self.client.get(url)
         self.assertContains(
             response, "about this practice")
