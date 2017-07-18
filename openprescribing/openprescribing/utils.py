f50b7960bf2d9f5aed205bb6381e4e91a6c03608
Peter Inglesby <peter.inglesby@gmail.com>
2017-07-18 14:31:01 +0100

    Add task for fetching prescribing data

---
+++ openprescribing/openprescribing/utils.py
@@ -0,0 +1,11 @@
+import os
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        import errno
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
