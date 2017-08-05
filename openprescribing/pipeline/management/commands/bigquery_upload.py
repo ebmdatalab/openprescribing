ec746b035be0cd65d22026464d387880c276bce5
Peter Inglesby <peter.inglesby@gmail.com>
2017-08-05 10:23:51 +0100

    Add sanity check that PracticeStatistics and Prescription tables are in sync
    Fixes #523

--- openprescribing/pipeline/management/commands/bigquery_upload.py
+++ openprescribing/pipeline/management/commands/bigquery_upload.py
@@ -1,14 +1,29 @@
 from __future__ import print_function
 
 from django.core.management import BaseCommand
+from django.db.models import Max
 
 from ebmdatalab import bigquery
+from frontend.models import PracticeStatistics, Prescription
 
 from ...cloud_utils import CloudHandler
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        # Make sure that PracticeStatistics and Prescription tables both have
+        # latest data.
+        latest_practice_statistic_date = PracticeStatistics.objects\
+            .aggregate(Max('date'))['date__max']
+        latest_prescription_date = Prescription.objects\
+            .aggregate(Max('processing_date'))['processing_date__max']
+
+        if latest_practice_statistic_date != latest_prescription_date:
+            msg = 'Latest PracticeStatistics object has date {}, '\
+                'while latest Prescription object has processing_date {}'\
+                .format(latest_practice_statistic_date, latest_prescription_date)
+            raise CommandError(msg)
+
         BigQueryUploader().update_bnf_table()
         bigquery.load_data_from_pg(
             'hscic', 'practices', 'frontend_practice',
