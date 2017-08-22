ebmdatalab/openprescribing

commit 4f30ba27c35be06a23de8e4fc694aeb3b2d85ea3

Fix progress bar for fetch_prescribing_data task
Fixes #550

openprescribing/pipeline/management/commands/fetch_prescribing_data.py
@@ -108,15 +108,12 @@ Specifically, you should:
 
         total_size = int(rsp.headers['content-length'])
 
-        with open(zip_path, 'wb') as f:
-            tqdm_iterator = tqdm(
-                rsp.iter_content(32 * 1024),
-                total=total_size,
-                unit='B',
-                unit_scale=True
-            )
-            for block in tqdm_iterator:
+        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+
+        with open('download.zip', 'wb') as f:
+            for block in rsp.iter_content(32 * 1024),
                 f.write(block)
+                progress_bar.update(len(block))
 
         with zipfile.ZipFile(zip_path) as zf:
             zf.extractall(dir_path)

commit 392b7935e980095a09be42415c6a6b00e6798222

Fix errors introduced in 4f30ba27

openprescribing/pipeline/management/commands/fetch_prescribing_data.py
@@ -142,8 +142,8 @@ Specifically, you should:
 
         progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
 
-        with open('download.zip', 'wb') as f:
-            for block in rsp.iter_content(32 * 1024),
+        with open(zip_path, 'wb') as f:
+            for block in rsp.iter_content(32 * 1024):
                 f.write(block)
                 progress_bar.update(len(block))
 