847f6f5bbaf7cf1267c3164e7c743daa80d521f1
Peter Inglesby <peter.inglesby@gmail.com>
2017-09-26 16:46:26 +0100

    Add new biquery.Client

---
+++ openprescribing/ebmdatalab/bigquery.py
@@ -0,0 +1,301 @@
+from contextlib import contextmanager
+import csv
+import os
+import re
+import subprocess
+import tempfile
+import time
+import uuid
+
+from google.cloud import bigquery as gcbq
+from google.cloud import storage as gcs
+from google.cloud.exceptions import Conflict
+
+from django.conf import settings
+from django.db import connection
+
+
+class Client(object):
+    def __init__(self, dataset_name):
+        # TODO: pass in credentials, rather than inferring from environment
+        self.project_name = settings.BQ_PROJECT
+        self.dataset_name = dataset_name
+
+        self.gcbq_client = gcbq.Client(project=self.project_name)
+        self.dataset = self.gcbq_client.dataset(dataset_name)
+
+    def list_jobs(self):
+        return self.gcbq_client.list_jobs()
+
+    def create_dataset(self):
+        self.dataset.create()
+
+    def delete_dataset(self):
+        for table in self.dataset.list_tables():
+            table.delete()
+        self.dataset.delete()
+
+    def create_table(self, table_name, schema):
+        table = self.dataset.table(table_name, schema)
+        table.create()
+        return Table(table, self.project_name)
+
+    def get_table_ref(self, table_name):
+        table = self.dataset.table(table_name)
+        return Table(table, self.project_name)
+
+    def get_table(self, table_name):
+        table = self.dataset.table(table_name)
+        table.reload()
+        return Table(table, self.project_name)
+
+    def get_or_create_table(self, table_name, schema):
+        try:
+            table = self.create_table(table_name, schema)
+        except Conflict:
+            table = self.get_table(table_name)
+        return table
+
+    def get_or_create_table_referencing_storage(self, table_name, schema, gcs_path):
+        gcs_client = gcs.client.Client(project=self.project_name)
+        bucket = gcs_client.bucket(self.project_name)
+        if bucket.get_blob(gcs_path) is None:
+            raise RuntimeError('Could not find blob at {}'.format(gcs_path))
+
+        gcs_uri = 'gs://{}/{}'.format(self.project_name, gcs_path)
+
+        resource = {
+            'tableReference': {'tableId': table_name},
+            'externalDataConfiguration': {
+                'csvOptions': {'skipLeadingRows': '1'},
+                'sourceFormat': 'CSV',
+                'sourceUris': [gcs_uri],
+                'schema': {'fields': schema}
+            }
+        }
+
+        path = '/projects/{}/datasets/{}/tables'.format(self.project_name, self.dataset_name)
+
+        try:
+            self.gcbq_client._connection.api_request(method='POST', path=path, data=resource)
+        except Conflict:
+            pass
+
+        return self.get_table(table_name)
+
+    def create_table_with_view(self, table_name, sql, legacy):
+        table = self.dataset.table(table_name)
+        table.view_query = sql
+        table.view_use_legacy_sql = legacy
+        table.create()
+        return Table(table, self.project_name)
+
+    def query(self, sql, legacy=False, **options):
+        if not legacy:
+            sql = convert_legacy_table_names(sql)
+
+        query = self.gcbq_client.run_sync_query(sql)
+        set_options(query, options)
+
+        query.run()
+
+        # The call to .run() might return before results are actually ready.
+        # See # https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query#timeoutMs
+        wait_for_job(query.job)
+
+        return query
+
+
+class Table(object):
+    def __init__(self, gcbq_table, project_name):
+        self.gcbq_table = gcbq_table
+        self.gcbq_client = gcbq_table._dataset._client
+        self.project_name = project_name
+        self.name = gcbq_table.name
+        self.dataset_name = gcbq_table._dataset.name
+
+    @property
+    def qualified_name(self):
+        return '{}.{}'.format(self.dataset_name, self.name)
+
+    @property
+    def legacy_full_qualified_name(self):
+        return '[{}:{}.{}]'.format(self.project_name, self.dataset_name, self.name)
+
+    def get_rows(self):
+        return self.gcbq_table.fetch_data()
+
+    def get_rows_as_dicts(self):
+        self.gcbq_table.reload()
+        field_names = [field.name for field in self.gcbq_table.schema]
+
+        for row in self.get_rows():
+            yield row_to_dict(row, field_names)
+
+    def insert_rows_from_query(self, sql, legacy=False, **options):
+        if not legacy:
+            sql = convert_legacy_table_names(sql)
+
+        default_options = {
+            'use_legacy_sql': legacy,
+            'allow_large_results': True,
+            'write_disposition': 'WRITE_TRUNCATE',
+            'destination': self.gcbq_table,
+        }
+
+        job = self.gcbq_client.run_async_query(
+            options.pop('job_name', gen_job_name()),
+            sql
+        )
+        set_options(job, options, default_options)
+
+        job.begin()
+
+        wait_for_job(job)
+
+    def insert_rows_from_csv(self, csv_path, **options):
+        default_options = {
+            'source_format': 'text/csv',
+            'write_disposition': 'WRITE_TRUNCATE',
+        }
+
+        merge_options(options, default_options)
+
+        with open(csv_path, 'rb') as f:
+            # This starts a job, so we don't need to call job.begin()
+            job = self.gcbq_table.upload_from_file(f, **options)
+
+        wait_for_job(job)
+
+    def insert_rows_from_storage(self, gcs_uri, **options):
+        default_options = {
+            'write_disposition': 'WRITE_TRUNCATE',
+        }
+
+        job = self.gcbq_client.load_table_from_storage(gen_job_name(), self.gcbq_table, gcs_uri)
+
+        set_options(job, options, default_options)
+
+        job.begin()
+
+        wait_for_job(job)
+
+
+class TableExporter(object):
+    def __init__(self, table, storage_prefix):
+        self.table = table
+        self.storage_prefix = storage_prefix
+        self.bucket = gcs.Client(project=table.project_name).bucket(table.project_name)
+
+    def export_to_storage(self, **options):
+        default_options = {
+            'compression': 'GZIP',
+        }
+
+        destination_uri = 'gs://{}/{}*.csv.gz'.format(
+            self.table.project_name,
+            self.storage_prefix,
+        )
+
+        job = self.table.gcbq_client.extract_table_to_storage(
+            options.pop('job_name', gen_job_name()),
+            self.table.gcbq_table,
+            destination_uri,
+        )
+
+        set_options(job, options, default_options)
+
+        job.begin()
+
+        wait_for_job(job)
+
+    def storage_blobs(self):
+        for blob in self.bucket.list_blobs(prefix=self.storage_prefix):
+            yield blob
+
+    def download_from_storage(self):
+        for blob in self.storage_blobs():
+            with tempfile.NamedTemporaryFile(mode='rb+') as f:
+                blob.download_to_file(f)
+                f.flush()
+                f.seek(0)
+                yield f
+
+    def download_from_storage_and_unzip(self, f_out):
+        for i, f_zipped in enumerate(self.download_from_storage()):
+            # Unzip
+            if i == 0:
+                cmd = "gunzip -c -f %s >> %s"
+            else:
+                # When the file is split into several shards in GCS, it
+                # puts a header on every file, so we have to skip that
+                # header on all except the first shard.
+                cmd = "gunzip -c -f %s | tail -n +2 >> %s"
+            subprocess.check_call(
+                cmd % (f_zipped.name, f_out.name), shell=True)
+
+    def delete_from_storage(self):
+        for blob in self.storage_blobs():
+            blob.delete()
+
+
+def wait_for_job(job, timeout_s=60):
+    t0 = time.time()
+
+    # Would like to use `while not job.done():` but cannot until we upgrade
+    # version of g.c.bq.
+    while True:
+        job.reload()
+        if job.state == 'DONE':
+            break
+
+        if time.time() - t0 > timeout_s:
+            raise TimeoutError
+        time.sleep(1)
+
+    if job.errors is not None:
+        raise JobError(job.errors)
+
+    # TODO Log time and cost
+
+
+class TimeoutError(StandardError):
+    pass
+
+
+class JobError(StandardError):
+    pass
+
+
+def convert_legacy_table_names(sql):
+    return re.sub(r'\[(.+?):(.+?)\.(.+?)\]', r'\1.\2.\3', sql)
+
+
+def set_options(thing, options, default_options=None):
+    if default_options is not None:
+        merge_options(options, default_options)
+    for k, v in options.items():
+        setattr(thing, k, v)
+        
+
+def merge_options(options, default_options):
+    for k, v in default_options.items():
+        options.setdefault(k, v)
+
+
+def gen_job_name():
+    return uuid.uuid4().hex
+
+
+def row_to_dict(row, field_names):
+    """Convert a row from bigquery into a dictionary, and convert NaN to
+    None
+
+    """
+    dict_row = {}
+    for value, field_name in zip(row, field_names):
+        if value and str(value).lower() == 'nan':
+            value = None
+        dict_row[field_name] = value
+    return dict_row
+
---
+++ openprescribing/ebmdatalab/tests/test_bigquery.py
@@ -0,0 +1,116 @@
+import csv
+import random
+import tempfile
+
+from google.cloud.bigquery import SchemaField
+from google.cloud import storage
+
+from django.test import TestCase
+
+from ebmdatalab.bigquery import Client, TableExporter
+
+
+class BQClientTest(TestCase):
+    def setUp(self):
+        self.dataset_name = 'bq_test_{:02d}'.format(random.randrange(100))
+
+        client = Client(self.dataset_name)
+        client.create_dataset()
+
+    def tearDown(self):
+        client = Client(self.dataset_name)
+        client.delete_dataset()
+
+    def test_the_lot(self):
+        client = Client(self.dataset_name)
+
+        schema = [
+            SchemaField('a', 'INTEGER'),
+            SchemaField('b', 'STRING'),
+        ]
+
+        headers = ['a', 'b']
+        rows = [
+            (1, 'apple'),
+            (2, 'banana'),
+            (3, 'coconut'),
+        ]
+
+        t1 = client.get_or_create_table('t1', schema)
+
+        # Test Table.insert_rows_from_csv
+        t1.insert_rows_from_csv('ebmdatalab/tests/test_table.csv')
+
+        self.assertEqual(sorted(t1.get_rows()), rows)
+
+        # Test Table.insert_rows_from_query
+        t2 = client.get_table_ref('t2')
+
+        t2.insert_rows_from_query('SELECT * FROM {} WHERE a > 1'.format(t1.qualified_name))
+
+        t2 = client.get_table('t2')
+
+        self.assertEqual(sorted(t2.get_rows()), rows[1:])
+
+        # Test Client.query
+        results = client.query('SELECT * FROM {} WHERE a > 2'.format(t1.qualified_name))
+
+        self.assertEqual(sorted(results.rows), rows[2:])
+
+        # Test TableExporter.export_to_storage and TableExporter.download_from_storage_and_unzip
+        t1_exporter = TableExporter(t1, 'test_bq_client/test_table-')
+        t1_exporter.export_to_storage()
+
+        with tempfile.NamedTemporaryFile(mode='r+') as f:
+            t1_exporter.download_from_storage_and_unzip(f)
+            f.seek(0)
+            reader = csv.reader(f)
+            data = [reader.next()] + sorted(reader)
+
+        self.assertEqual(data, [[str(x) for x in row] for row in [headers] + rows])
+
+        # Test Table.insert_rows_from_storage
+        self.upload_to_storage('ebmdatalab/tests/test_table.csv', 'test_bq_client/test_table.csv')
+
+        t2.insert_rows_from_storage('gs://ebmdatalab/test_bq_client/test_table.csv')
+
+        self.assertEqual(sorted(t2.get_rows()), rows)
+
+        # Test Client.get_or_create_table_referencing_storage
+        self.upload_to_storage('ebmdatalab/tests/test_table_headers.csv', 'test_bq_client/test_table_headers.csv')
+
+        schema = [
+            {'name': 'a', 'type': 'integer'},
+            {'name': 'b', 'type': 'string'},
+        ]
+
+        t3 = client.get_or_create_table_referencing_storage('t3', schema, 'test_bq_client/test_table_headers.csv')
+
+        results = client.query('SELECT * FROM {}'.format(t3.qualified_name))
+
+        self.assertEqual(sorted(results.rows), rows)
+
+        self.upload_to_storage(
+            'ebmdatalab/tests/test_table_headers_2.csv',
+            'test_bq_client/test_table_headers.csv'
+        )
+
+        results = client.query('SELECT * FROM {}'.format(t3.qualified_name))
+
+        self.assertEqual(sorted(results.rows), rows + [(4, u'damson')])
+
+        # Test Client.create_table_with_view
+        sql = 'SELECT * FROM {} WHERE a > 1'.format(t1.qualified_name)
+
+        t4 = client.create_table_with_view('t4', sql, False)
+
+        results = client.query('SELECT * FROM {}'.format(t4.qualified_name))
+
+        self.assertEqual(sorted(results.rows), rows[1:])
+
+    def upload_to_storage(self, local_path, storage_path):
+        client = storage.client.Client(project='ebmdatalab')
+        bucket = client.bucket('ebmdatalab')
+        blob = bucket.blob(storage_path)
+        with open(local_path) as f:
+            blob.upload_from_file(f)
