b8f6c8f85df453f3d27b878b566e91e3f2cd230f
Peter Inglesby <peter.inglesby@gmail.com>
2017-10-17 11:05:40 +0100

    Add fetcher and scraper for NCSO concessions
    There are some outstanding issues:
    
    * For any concessions whose drug name cannot be matched against dm+d
      data, we create a NCSOConcession object but do not match it against a
      DMDVmpp object.  We should decide what to do here.
    * The NCSOConcession model has a price_concession_pence attribute.  Is
      this the right way to record prices?
    * The DMDVmpp table already exists in the production database, so the
      migration that creates it should be skipped in production.  (Unless
      there's a better solution...)

---
+++ openprescribing/dmd/management/commands/fetch_ncso_concessions.py
@@ -0,0 +1,62 @@
+import calendar
+import csv
+import os
+
+import bs4
+import requests
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from openprescribing.utils import mkdir_p
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        url = 'http://psnc.org.uk/dispensing-supply/supply-chain/generic-shortages/ncso-archive/'
+        rsp = requests.get(url)
+        doc = bs4.BeautifulSoup(rsp.content, 'html.parser')
+
+        month_names = list(calendar.month_name)
+
+        for h2 in doc.findAll('h2', class_='trigger'):
+            month_name, year = h2.text.strip().split()
+            month = month_names.index(month_name)
+
+            year_and_month = '{}_{:02d}'.format(year, month)
+
+            if year_and_month < '2014_08':
+                break
+
+            dir_path = os.path.join(
+                settings.PIPELINE_DATA_BASEDIR,
+                'ncso_concessions',
+                year_and_month
+            )
+
+            if os.path.exists(dir_path):
+                continue
+
+            mkdir_p(dir_path)
+
+            table = h2.findNext('table')
+
+            records = []
+            for tr in table.findAll('tr'):
+                records.append([td.text for td in tr.findAll('td')])
+
+            # Make sure the first row contains expected headers.
+            # Unfortunately, the header names are not consistent.
+            assert 'drug' in records[0][0].lower()
+            assert 'pack' in records[0][1].lower()
+            assert 'price' in records[0][2].lower()
+
+            # Drop header row
+            records = records[1:]
+
+            file_name = 'ncso_concessions_{}.csv'.format(year_and_month)
+
+            with open(os.path.join(dir_path, file_name, 'w')) as f:
+                writer = csv.writer(f)
+                for record in records:
+                    writer.writerow(record)
--- openprescribing/dmd/tests.py
+++ openprescribing/dmd/tests.py
@@ -1,10 +1,12 @@
+# coding=utf8
+
 from mock import patch
 
 from django.db import connection
 from django.core.management import call_command
 from django.test import TestCase
 
-from dmd.models import DMDProduct
+from dmd.models import DMDProduct, DMDVmpp, NCSOConcession
 
 
 class CommandsTestCase(TestCase):
@@ -81,3 +83,51 @@ class CommandsTestCase(TestCase):
 
         amp = DMDProduct.objects.get(concept_class=2)
         self.assertEqual(amp.bnf_code, '0206020T0AAAGAG')
+
+    def test_import_ncso_concessions(self):
+        vmpp1 = DMDVmpp.objects.create(
+            vppid=1092811000001107,
+            nm='Amiloride 5mg tablets 100 tablet',
+        )
+        vmpp2 = DMDVmpp.objects.create(
+            vppid=8049011000001108,
+            nm='Duloxetine 40mg gastro-resistant capsules 56 capsule',
+        )
+
+        NCSOConcession.objects.create(
+            year_and_month='2017_01',
+            drug='Duloxetine 40mg capsules',
+            pack_size='56',
+            price_concession_pence=600,
+            vmpp_id=vmpp2.vppid,
+        )
+
+        path = 'dmd/tests/fixtures/commands/ncso_concessions_2017_02.csv'
+        call_command('import_ncso_concessions', '--filename', path)
+
+        cons1 = NCSOConcession.objects.get(
+            year_and_month='2017_02',
+            drug='Amiloride 5mg tablets'
+        )
+
+        cons2 = NCSOConcession.objects.get(
+            year_and_month='2017_02',
+            drug='Duloxetine 40mg gastro-resistant capsules'
+        )
+
+        cons3 = NCSOConcession.objects.get(
+            year_and_month='2017_02',
+            drug='Vitamin B Co Strong tablets'
+        )
+
+        self.assertEqual(cons1.pack_size, '100')
+        self.assertEqual(cons1.price_concession_pence, 925)
+        self.assertEqual(cons1.vmpp, vmpp1)
+
+        self.assertEqual(cons2.pack_size, '56')
+        self.assertEqual(cons2.price_concession_pence, 725)
+        self.assertEqual(cons2.vmpp, vmpp2)
+
+        self.assertEqual(cons3.pack_size, '28')
+        self.assertEqual(cons3.price_concession_pence, 525)
+        self.assertEqual(cons3.vmpp, None)
