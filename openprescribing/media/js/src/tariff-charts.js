783d5b0eaa8db0a741b084a46e79d3882a0be6e2
Seb Bacon <seb.bacon@gmail.com>
2017-12-04 14:13:38 +0000

    Lint javascript

--- openprescribing/media/js/src/tariff-charts.js
+++ openprescribing/media/js/src/tariff-charts.js
@@ -94,103 +94,93 @@ var tariffChart = {
     return newData;
   },
 
-    getChartTitle: function(graphType) {
-        var title = 'Total ';
-        title += (graphType === 'actual_cost') ? 'spending' : 'items';
-        if ((pageType !== 'ccg') && (pageType !== 'practice')) {
-            title += ' across all practices in England';
+  initialiseChartOptions: function(chartOptions, data) {
+    var _this = this;
+    var options = chartOptions.baseOptions;
+    options.chart.marginTop = 40;
+    options.plotOptions = {
+      series: {
+        marker: {
+          radius: 0,
+        },
+        fillOpacity: 0.4,
+        connectNulls: false,
+        pointPadding: 0,
+        groupPadding: 0,
+      },
+    };
+    options.chart.spacingTop = 20;
+    options.chart.type = 'area';
+    options.title.text = chartTitle;
+    options.legend = {
+      useHTML: true,
+      floating: false,
+      symbolHeight: 0,
+      symbolWidth: 0,
+      itemMarginTop: 4,
+      itemMarginBottom: 4,
+      labelFormatter: function() {
+        var str = '<div><div style="width:30px;display:inline-block;padding:3px 2px 3px 2px;margin-right: 4px;text-align:center;color:#FFF;background-color:' + this.color + '">';
+        if (this.name == 'Category A') {
+          str += '<svg width="30" height="5"><path d="M0 0 H30" stroke="black" stroke-width="2" stroke-dasharray="none" /></svg>';
+        } else if (this.name == 'Category C') {
+          str += '<svg width="30" height="5"><path d="M0 0 H30" stroke="black" stroke-width="2" stroke-dasharray="2,6" /></svg>';
+        } else if (this.name == 'Category M') {
+          str += '<svg width="30" height="5"><path d="M0 0 H30" stroke="black" stroke-width="2" stroke-dasharray="8,6" /></svg>';
         }
-        return title;
-    },
+        str += '</div>';
+        return str + this.name;
+      },
+    };
+    options.tooltip = {
+      pointFormatter: function() {
+        var str = '<span style="color:' + this.color + '">\u25CF</span> ';
+        str += this.series.name + ': <b>£' + this.y.toFixed(2);
+        if (this.tariff_category) {
+          str += ' (' + this.tariff_category + ') ';
+        }
+        str += '</b><br/>';
+        return str;
+      },
+    };
 
-
-    initialiseChartOptions: function(chartOptions, data) {
-      var _this = this;
-      var options = chartOptions.baseOptions;
-      options.chart.marginTop = 40;
-      options.plotOptions = {
-        series: {
-          marker: {
-            radius: 0
-          },
-          fillOpacity: 0.4,
-          connectNulls: false,
-          pointPadding: 0,
-          groupPadding: 0
-        }
-      };
-      options.chart.spacingTop = 20;
-      options.chart.type = 'area';
-      options.title.text = chartTitle;
-      options.legend = {
-        useHTML: true,
-        floating: false,
-        symbolHeight: 0,
-        symbolWidth: 0,
-        itemMarginTop: 4,
-        itemMarginBottom: 4,
-        labelFormatter: function() {
-          var str = '<div><div style="width:30px;display:inline-block;padding:3px 2px 3px 2px;margin-right: 4px;text-align:center;color:#FFF;background-color:' + this.color + '">';
-          if (this.name == 'Category A') {
-            str += '<svg width="30" height="5"><path d="M0 0 H30" stroke="black" stroke-width="2" stroke-dasharray="none" /></svg>';
-          } else if (this.name == 'Category C') {
-            str += '<svg width="30" height="5"><path d="M0 0 H30" stroke="black" stroke-width="2" stroke-dasharray="2,6" /></svg>';
-          } else if (this.name == 'Category M') {
-            str += '<svg width="30" height="5"><path d="M0 0 H30" stroke="black" stroke-width="2" stroke-dasharray="8,6" /></svg>';
-          }
-          str += '</div>'
-          return str + this.name;
-        }
-      }
-      options.tooltip = {
-        pointFormatter: function() {
-          var str = '<span style="color:' + this.color + '">\u25CF</span> ';
-          str += this.series.name + ': <b>£' + this.y.toFixed(2);
-          if (this.tariff_category) {
-            str += ' (' + this.tariff_category + ') ';
-          }
-          str += '</b><br/>';
-          return str;
-        }
-      }
-
-      if (data.length > 1) {
-        options.legend.enabled = true;
-      } else {
-        options.legend.enabled = false;
-      }
-        options.yAxis.title = {
-            text: "Price (£)"
-        };
-        return options;
-    },
+    if (data.length > 1) {
+      options.legend.enabled = true;
+    } else {
+      options.legend.enabled = false;
+    }
+    options.yAxis.title = {
+      text: 'Price (£)'
+    };
+    return options;
+  },
 
   setUp: function() {
     var _this = this;
     $('.tariff-selector').select2(
       {placeholder: 'Start typing a presentation name'}
     );
-        $.ajax({
-          type: "GET",
-          url: filename,
-          error: function() {
-            $('.status').html('<p>Sorry, something went wrong.</p>');
-          },
-          success: function(response) {
-            $('.status').hide();
-            var data = _this.initialiseData(response);
-            chartOptions = _this.initialiseChartOptions(chartOptions, data);
-            if (data.length) {
-                $('#tariff').show();
-                chartOptions.series = data;
-                var chart = new Highcharts.Chart(chartOptions);
-            } else {
-                $('#tariff').hide();
-                $('#no-data').show();
-            }
-          }
-        });
-    }
+    $.ajax({
+      type: 'GET',
+      url: filename,
+      error: function() {
+        $('.status').html('<p>Sorry, something went wrong.</p>');
+      },
+      success: function(response) {
+        $('.status').hide();
+        var data = _this.initialiseData(response);
+        chartOptions = _this.initialiseChartOptions(chartOptions, data);
+        if (data.length) {
+          $('#tariff').show();
+          chartOptions.series = data;
+          var chart = new Highcharts.Chart(chartOptions);
+        } else {
+          $('#tariff').hide();
+          $('#no-data').show();
+        }
+      },
+    });
+  },
 };
 
 $(document).ready(function() {
