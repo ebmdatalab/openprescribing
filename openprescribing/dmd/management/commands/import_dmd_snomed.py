b3daa8ae9b28b226dd7bdf96941b5c3e0a4b1ce2
Peter Inglesby <peter.inglesby@gmail.com>
2018-01-03 11:20:35 +0000

    Ensure import_dmd_snomed always runs (#710)

--- openprescribing/dmd/management/commands/import_dmd_snomed.py
+++ openprescribing/dmd/management/commands/import_dmd_snomed.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from openpyxl import load_workbook
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import connection, transaction
 
@@ -11,10 +12,19 @@ class Command(BaseCommand):
             'accordingly.')
 
     def add_arguments(self, parser):
-        parser.add_argument('--filename', required=True)
+        parser.add_argument('--filename')
 
     def handle(self, *args, **options):
-        wb = load_workbook(filename=os.path.join(options['filename']))
+        filename = options['filename']
+
+        if filename is None:
+            base_dirname = os.path.join(settings.PIPELINE_DATA_BASEDIR, 'dmd_snomed')
+            dirname = sorted(os.listdir(base_dirname))[-1]
+            filenames = os.listdir(os.path.join(base_dirname, dirname))
+            assert len(filenames) == 1
+            filename = os.path.join(base_dirname, dirname, filenames[0])
+
+        wb = load_workbook(filename=filename)
         rows = wb.active.rows
         with transaction.atomic():
             with connection.cursor() as cursor:
