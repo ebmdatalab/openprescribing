51dceee430f6dd9fb169eb8a610ae9470479fba9
Peter Inglesby <peter.inglesby@gmail.com>
2018-02-01 13:08:11 +0000

    Add comment about where opioid_measure data comes from

--- openprescribing/frontend/management/commands/measure_sql/opioid_total_ome.sql
+++ openprescribing/frontend/management/commands/measure_sql/opioid_total_ome.sql
@@ -6,6 +6,8 @@ SELECT
 FROM
   {project}.{hscic}.normalised_prescribing_standard AS presc
 JOIN
+  -- data in richard.opioid_measure comes from:
+  -- https://docs.google.com/spreadsheets/d/1IjnHbYVszZKPmVSYydtMVzbDLPOmq8bOFq45QsSu6sE/edit?usp=sharing
   ebmdatalab.richard.opioid_measure as opioid
 ON
   CONCAT(SUBSTR(presc.bnf_code,0,9),'AA%',(SUBSTR(presc.bnf_code,-2,2)))=CONCAT(SUBSTR(opioid.bnf_code,0,11),'%',(SUBSTR(opioid.bnf_code,-2,2)))
