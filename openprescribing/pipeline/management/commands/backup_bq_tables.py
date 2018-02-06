ebmdatalab/openprescribing

commit 4a010e69f36fecdbe6e1433f3314d23de31d305b

Back up practice_statistics table

openprescribing/pipeline/management/commands/backup_bq_tables.py
@@ -9,27 +9,32 @@ from gcutils.storage import Client as StorageClient
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        self.backup_table('prescribing')
+        self.backup_table('practice_statistics')
+
+    def backup_table(table_name):
         client = Client('hscic')
-        sql = 'SELECT max(month) FROM {hscic}.prescribing'
+        sql = 'SELECT max(month) FROM {hscic}.%s' % table_name
         latest_date = client.query(sql).rows[0][0]
         latest_year_and_month = latest_date.strftime('%Y_%m')
-        table = client.get_table('prescribing')
+        table = client.get_table(table_name)
 
         storage_client = StorageClient()
         bucket = storage_client.bucket()
         year_and_months = set()
 
-        prefix_base = 'prescribing/prescribing_backups/'
+        prefix_base = 'backups/{}/'.format(table_name)
 
         for blob in bucket.list_blobs(prefix=prefix_base):
             match = re.search('/(\d{4}_\d{2})-', blob.name)
             year_and_months.add(match.groups()[0])
 
         if latest_year_and_month in year_and_months:
-            print 'Prescribing table already backed up for {}'. \
-                format(latest_year_and_month)
+            print '{} table already backed up for {}'. \
+                format(table_name, latest_year_and_month)
             return
 
-        storage_prefix = '{}/{}-'.format(prefix_base, latest_year_and_month)
+        storage_prefix = '{}/{}/{}-'.format(
+            prefix_base, latest_year_and_month, table_name)
         exporter = TableExporter(table, storage_prefix)
         exporter.export_to_storage()

commit 336b2950a6b2b8e2a203ba829e1fb7a3620c37a5

Fix regex in backup_bq_tables
Bug introduced in 4a010e6

openprescribing/pipeline/management/commands/backup_bq_tables.py
@@ -26,7 +26,7 @@ class Command(BaseCommand):
         prefix_base = 'backups/{}/'.format(table_name)
 
         for blob in bucket.list_blobs(prefix=prefix_base):
-            match = re.search('/(\d{4}_\d{2})-', blob.name)
+            match = re.search('/(\d{4}_\d{2})/', blob.name)
             year_and_months.add(match.groups()[0])
 
         if latest_year_and_month in year_and_months: