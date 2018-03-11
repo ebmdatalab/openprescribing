2cd16e8f5f0d7d1d97936843b6d80154118f9890
Peter Inglesby <peter.inglesby@gmail.com>
2018-03-11 22:15:36 +0000

    Fix redirection in crontab
    cron's using /bin/sh, which can't parse $()

--- deploy/run_pipeline_e2e_tests.sh
+++ deploy/run_pipeline_e2e_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=openprescribing.settings.e2etest
+LOGFILE=$(mktemp -t e2e-$(date +%Y-%m-%d)-XXXX.log)
 
 . /webapps/openprescribing/.venv/bin/activate
 
 PGPASSWORD=$DB_PASS dropdb -U $DB_USER $E2E_DB_NAME 
 PGPASSWORD=$DB_PASS createdb -U $DB_USER $E2E_DB_NAME 
 
-python /webapps/openprescribing/openprescribing/manage.py migrate
-
-python /webapps/openprescribing/openprescribing/manage.py pipeline_e2e_tests
+python /webapps/openprescribing/openprescribing/manage.py migrate >$LOGFILE
+python /webapps/openprescribing/openprescribing/manage.py pipeline_e2e_tests >$LOGFILE
