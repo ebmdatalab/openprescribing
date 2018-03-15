2f3a9cbd37bface87475a3b72d956a8c6a522ec9
Peter Inglesby <peter.inglesby@gmail.com>
2018-03-15 11:24:34 +0000

    Redirect logs properly

--- deploy/run_pipeline_e2e_tests.sh
+++ deploy/run_pipeline_e2e_tests.sh
@@ -7,5 +7,5 @@ LOGFILE=$(mktemp -t e2e-$(date +%Y-%m-%d)-XXXX.log)
 PGPASSWORD=$DB_PASS dropdb -U $DB_USER $E2E_DB_NAME 
 PGPASSWORD=$DB_PASS createdb -U $DB_USER $E2E_DB_NAME 
 
-python /webapps/openprescribing/openprescribing/manage.py migrate 2>&1 >$LOGFILE
-python /webapps/openprescribing/openprescribing/manage.py pipeline_e2e_tests 2>&1 >$LOGFILE
+python /webapps/openprescribing/openprescribing/manage.py migrate >$LOGFILE 2>&1
+python /webapps/openprescribing/openprescribing/manage.py pipeline_e2e_tests >$LOGFILE 2>&1
