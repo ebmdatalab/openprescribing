16ce875a29ffe7182554cec9e24eba808978f4a5
Peter Inglesby <peter.inglesby@gmail.com>
2018-05-16 19:18:58 +0100

    Update tests to reflect fixture changes

--- openprescribing/frontend/tests/test_api_org_details.py
+++ openprescribing/frontend/tests/test_api_org_details.py
@@ -96,40 +96,40 @@ class TestAPIOrgDetailsViews(ApiTestBase):
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         rows = list(csv.DictReader(response.content.splitlines()))
-        self.assertEqual(len(rows), 4)
-        self.assertEqual(rows[0]['row_id'], 'N84014')
-        self.assertEqual(rows[0]['row_name'], 'AINSDALE VILLAGE SURGERY')
+        self.assertEqual(len(rows), 10)  # 5 practices, 2 months
+        self.assertEqual(rows[0]['row_id'], 'B82018')
+        self.assertEqual(rows[0]['row_name'], 'ESCRICK SURGERY')
         self.assertEqual(rows[0]['date'], '2015-01-01')
-        self.assertEqual(float(rows[0]['total_list_size']), 25)
-        self.assertEqual(rows[0]['astro_pu_cost'], '489.7')
-        self.assertEqual(rows[0]['astro_pu_items'], '819.2')
-        self.assertEqual(rows[0]['star_pu.oral_antibacterials_item'],
-                         '10')
-        self.assertEqual(rows[3]['row_id'], 'P87629')
-        self.assertEqual(rows[3]['date'], '2015-02-01')
-        self.assertEqual(float(rows[3]['total_list_size']), 29)
-        self.assertEqual(rows[3]['astro_pu_items'], '1600.2')
-        self.assertEqual(rows[3]['star_pu.oral_antibacterials_item'],
-                         '29')
+        self.assertEqual(rows[0]['total_list_size'], '324')
+        self.assertEqual(rows[0]['astro_pu_cost'], '181.1')
+        self.assertEqual(rows[0]['astro_pu_items'], '271.1')
+        self.assertEqual(rows[0]['star_pu.oral_antibacterials_item'], '27.1')
 
     def test_api_view_org_details_ccg_code_to_practices(self):
+        # Practice K83622 moved from 03Q to 03V so we check that it is only
+        # included in the results for 03V and not 03Q.
+        url = self.api_prefix
+        url += '/org_details?format=csv&org_type=practice&org=03V'
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        rows = list(csv.DictReader(response.content.splitlines()))
+        self.assertEqual(len(rows), 6)  # 3 practices, 2 months
+        self.assertIn('K83622', [row['row_id'] for row in rows])
+        self.assertEqual(rows[0]['row_id'], 'K83059')
+        self.assertEqual(rows[0]['row_name'], 'DR KHALID & PARTNERS')
+        self.assertEqual(rows[0]['date'], '2015-01-01')
+        self.assertEqual(rows[0]['total_list_size'], '216')
+        self.assertEqual(rows[0]['astro_pu_cost'], '121.1')
+        self.assertEqual(rows[0]['astro_pu_items'], '151.1')
+        self.assertEqual(rows[0]['star_pu.oral_antibacterials_item'], '15.1')
+
         url = self.api_prefix
         url += '/org_details?format=csv&org_type=practice&org=03Q'
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         rows = list(csv.DictReader(response.content.splitlines()))
-        # Although in the practicestatistics fixtures, N84014 was in
-        # 03Q in only one month, we want to return all practices as if
-        # they were always in their current CCG membership
-        self.assertEqual(len(rows), 2)
-        self.assertEqual(rows[0]['row_id'], 'N84014')
-        self.assertEqual(rows[0]['row_name'], 'AINSDALE VILLAGE SURGERY')
-        self.assertEqual(rows[0]['date'], '2015-01-01')
-        self.assertEqual(float(rows[0]['total_list_size']), 25)
-        self.assertEqual(rows[0]['astro_pu_cost'], '489.7')
-        self.assertEqual(rows[0]['astro_pu_items'], '819.2')
-        self.assertEqual(rows[0]['star_pu.oral_antibacterials_item'],
-                         '10')
+        self.assertEqual(len(rows), 4)  # 2 practices, 2 months
+        self.assertNotIn('K83622', [row['row_id'] for row in rows])
 
     def test_api_view_org_details_one_practice(self):
         url = self.api_prefix
@@ -137,15 +137,14 @@ class TestAPIOrgDetailsViews(ApiTestBase):
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         rows = list(csv.DictReader(response.content.splitlines()))
-        self.assertEqual(len(rows), 2)
+        self.assertEqual(len(rows), 2)  # 2 months
         self.assertEqual(rows[0]['row_id'], 'N84014')
         self.assertEqual(rows[0]['row_name'], 'AINSDALE VILLAGE SURGERY')
         self.assertEqual(rows[0]['date'], '2015-01-01')
-        self.assertEqual(float(rows[0]['total_list_size']), 25)
-        self.assertEqual(rows[0]['astro_pu_cost'], '489.7')
-        self.assertEqual(rows[0]['astro_pu_items'], '819.2')
-        self.assertEqual(rows[0]['star_pu.oral_antibacterials_item'],
-                         '10')
+        self.assertEqual(rows[0]['total_list_size'], '288')
+        self.assertEqual(rows[0]['astro_pu_cost'], '161.1')
+        self.assertEqual(rows[0]['astro_pu_items'], '231.1')
+        self.assertEqual(rows[0]['star_pu.oral_antibacterials_item'], '23.1')
 
     def test_api_view_org_details_one_practice_with_json_key(self):
         url = self.api_prefix
@@ -154,13 +153,12 @@ class TestAPIOrgDetailsViews(ApiTestBase):
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         rows = list(csv.DictReader(response.content.splitlines()))
-        self.assertEqual(len(rows), 2)
+        self.assertEqual(len(rows), 2)  # 2 months
         self.assertEqual(rows[0]['row_id'], 'N84014')
         self.assertEqual(rows[0]['row_name'], 'AINSDALE VILLAGE SURGERY')
         self.assertEqual(rows[0]['date'], '2015-01-01')
-        self.assertEqual(float(rows[0]['total_list_size']), 25)
-        self.assertEqual(rows[0]['star_pu.oral_antibacterials_item'],
-                         '10')
+        self.assertEqual(rows[0]['total_list_size'], '288')
+        self.assertEqual(rows[0]['star_pu.oral_antibacterials_item'], '23.1')
         self.assertEqual(rows[0].get('astro_pu_cost'), None)
 
     def test_api_view_org_details_all_nhs_with_json_key(self):
