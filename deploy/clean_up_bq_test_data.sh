7d6e59a3b5fbcf1b93427604b3777368f31702b8
Peter Inglesby <peter.inglesby@gmail.com>
2018-06-13 09:37:10 +0100

    Redirect clean_up_bq_test_data logs
    This will stop us getting an email every time it runs overnight

--- deploy/clean_up_bq_test_data.sh
+++ deploy/clean_up_bq_test_data.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=openprescribing.settings.test
+LOGFILE=$(mktemp -t clean_up_bq_test_data-$(date +%Y-%m-%d)-XXXX.log)
+
 . /webapps/openprescribing/.venv/bin/activate
-python /webapps/openprescribing/openprescribing/manage.py clean_up_bq_test_data
+
+python /webapps/openprescribing/openprescribing/manage.py clean_up_bq_test_data >$LOGFILE 2>&1
