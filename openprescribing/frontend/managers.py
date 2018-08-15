67bf0c7149c176a169446a809a2e5269df259e0c
David Evans <dave@evans.io>
2018-08-15 17:55:46 +0100

    Support summing measures over all CCGs/practices
    This extends the `measure_by_ccg` and `measure_by_practice` endpoints to
    accept a pseudo organisation ID of "_all" which returns measures
    calcuated over all CCGs/practices. More precisely:
    
     * Numerators and denominators are summed and the ratio is
       recalculated. (Note there are some denominators like STAR-PUs where
       summing doesn't really make sense, but hey ho.)
    
     * Cost savings for each centile are summed, but with negative values
       ignored. Thus the aggregate cost saving at the median represents the
       saving if all CCGs worse than the median improved and all CCGs better
       than the median stayed the same, rather than saving to be had if all
       CCGs converged on the median, which is what the simple sum would give
       us.
    
    This "pseudo ID" technique is obviously a bit of hack, but it allows us
    to get the data we want without rewriting or extending large chunks of
    the API or the frontend code.

--- openprescribing/frontend/managers.py
+++ openprescribing/frontend/managers.py
@@ -1,9 +1,21 @@
-from django.db.models import Q
 from django.db import models
+from django.db.models import FloatField, Func, Q, Value, Sum
+from django.db.models.functions import Cast, Coalesce, Greatest
+from django.contrib.postgres.fields.jsonb import (
+    JSONField, KeyTextTransform as ValueFromJsonObject
+)
+
+
+ALL_ORGS_PSEUDO_ID = '_all'
+ALL_PRACTICE_NAME = 'All practices in England'
+ALL_PCT_NAME = 'All CCGs in England'
+
+CENTILES = ['10', '20', '30', '40', '50', '60', '70', '80', '90']
 
 
 class MeasureValueManager(models.Manager):
     def by_ccg(self, org_ids, measure_ids=None, tags=None):
+        org_ids, aggregate_all = _extract_all_orgs_pseudo_id(org_ids)
         org_Q = Q()
         for org_id in org_ids:
             org_Q |= Q(pct_id=org_id)
@@ -23,9 +35,12 @@ class MeasureValueManager(models.Manager):
         if tags:
             qs = qs.filter(measure__tags__contains=tags)
 
+        if aggregate_all:
+            qs = _aggregate_all_results_by_measure(qs, include_practice=False)
         return qs
 
     def by_practice(self, org_ids, measure_ids=None, tags=None):
+        org_ids, aggregate_all = _extract_all_orgs_pseudo_id(org_ids)
         org_Q = Q()
         for org_id in org_ids:
             if len(org_id) == 3:
@@ -46,4 +61,126 @@ class MeasureValueManager(models.Manager):
         if tags:
             qs = qs.filter(measure__tags__contains=tags)
 
+        if aggregate_all:
+            qs = _aggregate_all_results_by_measure(qs, include_practice=True)
         return qs
+
+
+def _extract_all_orgs_pseudo_id(org_ids):
+    aggregate_all = False
+    if ALL_ORGS_PSEUDO_ID in org_ids:
+        if len(org_ids) != 1:
+            raise ValueError(
+                'Queries for "{}" cannot be combined with other '
+                'org_ids'.format(ALL_ORGS_PSEUDO_ID))
+        aggregate_all = True
+        org_ids = ()
+    return org_ids, aggregate_all
+
+
+def _aggregate_all_results_by_measure(queryset, include_practice=False):
+    # Avoid circular imports by getting model references from queryset
+    MeasureValue = queryset.model
+    Practice = MeasureValue.practice.field.related_model
+    PCT = MeasureValue.pct.field.related_model
+
+    pseudo_practice = Practice(name=ALL_PRACTICE_NAME)
+    pseudo_practice.id = ALL_ORGS_PSEUDO_ID
+    pseudo_pct = PCT(name=ALL_PCT_NAME)
+    pseudo_pct.id = ALL_ORGS_PSEUDO_ID
+
+    aggregate_queryset = (
+        queryset
+        .values('measure_id', 'month')
+        .order_by('measure_id', 'month')
+        .annotate(
+            numerator=Sum('numerator'),
+            denominator=Sum('denominator'),
+            calc_value=_divide('numerator', 'denominator'),
+            cost_savings=_aggregate_over_json(
+                field='cost_savings',
+                aggregate_function=_sum_positive_values,
+                keys=CENTILES
+            ),
+        )
+    )
+
+    for item in aggregate_queryset:
+        measure_value = MeasureValue(**item)
+        if include_practice:
+            measure_value.practice = pseudo_practice
+            measure_value.practice_id = pseudo_practice.id
+        else:
+            measure_value.pct = pseudo_pct
+            measure_value.pct_id = pseudo_pct.id
+        yield measure_value
+
+
+def _divide(numerator_field, denominator_field):
+    """
+    SQL division function which handles NULLs and divide-by-zero gracefully
+    """
+    numerator = Coalesce(numerator_field, Value(0.0))
+    denominator = Func(denominator_field, Value(0.0), function='NULLIF')
+    return numerator / denominator
+
+
+def _aggregate_over_json(field, aggregate_function, keys):
+    """
+    SQL function which aggregates over `field` (which should be of JSONB type)
+    by applying `aggregate_function` over each of the supplied `keys` within
+    the object.  Returns a JSONB object with those specified keys.
+    """
+    return _build_json_object({
+        key: aggregate_function(_get_json_value(key, field))
+        for key in keys
+    })
+
+
+def _build_json_object(dictionary):
+    """
+    SQL function which builds a JSONB object from a Python dictionary whose
+    keys are fixed values (strings or ints) but whose values may be arbitrary
+    SQL functions
+    """
+    args = []
+    for key, value in dictionary.items():
+        args.append(Value(_preserve_key_type(key)))
+        args.append(value)
+    return Func(*args, function='jsonb_build_object', output_field=JSONField())
+
+
+def _sum_positive_values(field):
+    """
+    SQL function which sums over `field` ignoring all negative values
+    """
+    field_as_float = Cast(field, FloatField())
+    field_with_floor = Greatest(field_as_float, Value(0.0))
+    return Sum(field_with_floor)
+
+
+def _get_json_value(key, json_field):
+    """
+    SQL function which extracts `key` from a field of type JSONB
+    """
+    return ValueFromJsonObject(_preserve_key_type(key), json_field)
+
+
+def _preserve_key_type(key):
+    """
+    Django's Postgres JSON library always converts int-like strings to ints. If
+    you want to read from an object with a key of "10" (the string) you are
+    screwed. Hence this nasty workaround.
+    """
+    if not isinstance(key, (int, NotAnInt)):
+        key = NotAnInt(key)
+    return key
+
+
+class NotAnInt(str):
+    """
+    Represents a string that can't be converted to an int, even if it looks
+    like one
+    """
+    def __int__(self):
+        raise ValueError('Not an int')
