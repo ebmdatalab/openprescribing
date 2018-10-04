6674ad8910091d67afccfe414ee75d2872434b53
Seb Bacon <seb.bacon@gmail.com>
2018-10-04 11:42:21 +0100

    New functional tests (and fixes to make them pass)

--- openprescribing/frontend/tests/commands/test_import_adqs.py
+++ openprescribing/frontend/tests/commands/test_import_adqs.py
@@ -5,6 +5,12 @@ from django.core.management import call_command
 from django.test import TestCase
 from frontend.models import Presentation
 
+from gcutils.bigquery import Client
+from gcutils.bigquery import NotFound
+
+from frontend.bq_schemas import RAW_PRESCRIBING_SCHEMA
+from frontend.models import ImportLog
+
 
 class CommandsTestCase(TestCase):
     fixtures = ['presentations', 'importlog']
@@ -12,7 +18,7 @@ class CommandsTestCase(TestCase):
     @patch('gcutils.bigquery.Client')
     def test_import_adqs(self, mock_client):
         mock_query = MagicMock(name='query')
-        mock_query.query.return_value = [
+        mock_query.query.return_value.rows = [
             ('0202010F0.*AA', 0.333),
         ]
         mock_client.return_value = mock_query
@@ -29,3 +35,38 @@ class CommandsTestCase(TestCase):
         self.assertEqual(p.adq_per_quantity, 0.333)
         p = Presentation.objects.get(bnf_code='0204000I0JKKKAL')
         self.assertEqual(p.adq_per_quantity, None)
+
+
+class CommandsFunctionalTestCase(TestCase):
+    fixtures = ['presentations', 'importlog']
+
+    def setUp(self):
+        """Create a raw_prescribing_data table such as is expected to exist by
+        ADQ calculation code.
+        """
+        raw_data_path = 'frontend/tests/fixtures/commands/' +\
+                        'convert_hscic_prescribing/2016_01/' +\
+                        'Detailed_Prescribing_Information.csv'
+        year_and_month = ImportLog.objects.latest_in_category(
+            'prescribing').current_at.strftime("%Y_%m")
+        self.table_name = 'raw_prescribing_data_{}'.format(year_and_month)
+        self.client = Client('tmp_eu')
+        t1 = self.client.get_or_create_table(
+            self.table_name, RAW_PRESCRIBING_SCHEMA)
+        t1.insert_rows_from_csv(raw_data_path, skip_leading_rows=1)
+
+        call_command('import_adqs')
+
+    def test_import_adqs_functional(self):
+        # These all have ADQs in the raw data
+        for p in Presentation.objects.filter(bnf_code__regex='0202010B0.*AB'):
+            self.assertEqual(p.adq_per_quantity, 1.0)
+        # This doesn't exist in the raw data
+        p = Presentation.objects.get(bnf_code='0204000I0AAAZAZ')
+        self.assertEqual(p.adq_per_quantity, None)
+
+    def tearDown(self):
+        try:
+            self.client.delete_table(self.table_name)
+        except NotFound:
+            pass
