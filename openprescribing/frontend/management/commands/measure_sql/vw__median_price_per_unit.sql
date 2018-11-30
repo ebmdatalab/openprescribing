b3c8dc6f838910ff2aeda34c2691851a8b4deada
Peter Inglesby <peter.inglesby@gmail.com>
2018-11-30 15:55:44 +0000

    Add SQL for new measure view

---
+++ openprescribing/frontend/management/commands/measure_sql/vw__median_price_per_unit.sql
@@ -0,0 +1,18 @@
+WITH
+  prices_per_unit AS (
+  SELECT
+    month AS date,
+    bnf_code,
+    IEEE_DIVIDE(net_cost,quantity) AS price_per_unit
+  FROM
+    ebmdatalab.hscic.normalised_prescribing_standard)
+SELECT
+  DISTINCT date,
+  bnf_code,
+  percentile_cont(price_per_unit,
+    0.5) OVER (PARTITION BY date, bnf_code) AS median_price_per_unit
+FROM
+  prices_per_unit
+ORDER BY
+  bnf_code,
+  date
