6f584a2deb338348c75b22131c767c76045cb3ee
Peter Inglesby <peter.inglesby@gmail.com>
2018-12-08 21:22:31 +0000

    Add as_json templatetag

--- openprescribing/frontend/templatetags/template_extras.py
+++ openprescribing/frontend/templatetags/template_extras.py
@@ -1,3 +1,4 @@
+import json
 import math
 
 from django import template
@@ -107,3 +108,8 @@ def fancy_join(lst, sep=', ', final_sep=' and '):
         head, tail = lst[:-1], lst[-1]
         lst = [sep.join(head), tail]
     return final_sep.join(lst)
+
+
+@register.filter(is_safe=True)
+def as_json(obj):
+    return mark_safe(json.dumps(obj, indent=2))
