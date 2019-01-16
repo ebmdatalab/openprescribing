755cc8853a11f52eea279862496a298f9a004373
Peter Inglesby <peter.inglesby@gmail.com>
2019-01-16 09:16:51 +0000

    Update import_snomed_mapping.py to handle new mapping format

--- openprescribing/dmd2/management/commands/import_snomed_mapping.py
+++ openprescribing/dmd2/management/commands/import_snomed_mapping.py
@@ -1,7 +1,7 @@
-import csv
 from datetime import datetime
 from django.core.management import BaseCommand
 from django.db import connection, transaction
+from openpyxl import load_workbook
 
 from dmd2 import models
 
@@ -12,35 +12,49 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         type_to_model = {
-            ('VMP', 'Presentation'): models.VMP,
-            ('AMP', 'Presentation'): models.AMP,
-            ('VMP', 'Pack'): models.VMPP,
-            ('AMP', 'Pack'): models.AMPP,
+            ('Presentation', 'VMP'): models.VMP,
+            ('Presentation', 'AMP'): models.AMP,
+            ('Pack', 'VMP'): models.VMPP,
+            ('Pack', 'AMP'): models.AMPP,
         }
 
-        with open(kwargs['path']) as f:
-            reader = csv.DictReader(f)
+        wb = load_workbook(filename=kwargs['path'])
+        rows = wb.active.rows
 
-            with transaction.atomic():
-                for ix, row in enumerate(reader):
-                    if ix % 10000 == 0:
-                        print('{} {}'.format(datetime.now(), ix))
-                    model = type_to_model[(row['VMP_AMP'], row['PACK_PRESENTATION'])]
-                    snomed_id = row['SNOMED_CONCEPT_ID'].lstrip("'")
-                    bnf_code = row['BNF_CODE'].lstrip("'")
+        headers = next(rows)
+        assert headers[0].value == 'Presentation / Pack Level'
+        assert headers[1].value == 'VMP / AMP'
+        assert headers[2].value == 'BNF Code'
+        assert headers[4].value == 'SNOMED Code'
 
-                    if snomed_id == '':
-                        # print('No SNOMED for {} {} {}'.format(row['VMP_AMP'], row['PACK_PRESENTATION'], bnf_code))
-                        continue
+        with transaction.atomic():
+            models.VMP.objects.update(bnf_code=None)
+            models.AMP.objects.update(bnf_code=None)
+            models.VMPP.objects.update(bnf_code=None)
+            models.AMPP.objects.update(bnf_code=None)
 
-                    if bnf_code == '':
-                        # print('No BNF code for {} {} {}'.format(row['VMP_AMP'], row['PACK_PRESENTATION'], snomed_id))
-                        continue
+            for ix, row in enumerate(rows):
+                if ix % 10000 == 0:
+                    print('{} {}'.format(datetime.now(), ix))
 
-                    try:
-                        obj = model.objects.get(id=snomed_id)
-                    except model.DoesNotExist:
-                        print('Could not get {} with id {}'.format(model.__name__, snomed_id))
-                        continue
-                    obj.bnf_code = bnf_code
-                    obj.save()
+                model = type_to_model[(row[0].value, row[1].value)]
+
+                bnf_code = row[2].value
+                snomed_id = row[4].value
+
+                if bnf_code is None or snomed_id is None:
+                    continue
+
+                if bnf_code == "'" or snomed_id == "'":
+                    continue
+
+                bnf_code = bnf_code.lstrip("'")
+                snomed_id = snomed_id.lstrip("'")
+
+                try:
+                    obj = model.objects.get(id=snomed_id)
+                except model.DoesNotExist:
+                    print('Could not get {} with id {}'.format(model.__name__, snomed_id))
+                    continue
+                obj.bnf_code = bnf_code
+                obj.save()
