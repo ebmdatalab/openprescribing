571d901207d6d7f5a8132db0881076660ac792f5
Seb Bacon <seb.bacon@gmail.com>
2019-01-23 15:54:21 +0000

    Address review feedback

--- openprescribing/dmd2/views.py
+++ openprescribing/dmd2/views.py
@@ -194,8 +194,8 @@ def vmp_relationships_view(request, vmp_id):
 
     ctx = {
         'vmp': vmp,
-        'vmpp_cols': len(vmpps),
-        'amp_rows': len(amps),
+        'num_vmpps': len(vmpps),
+        'num_amps': len(amps),
         'table': table,
         'presentations': presentations,
         'vmp_presentation': vmp_presentation,
