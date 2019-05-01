9cf8b8c77eb0c77b3563dc987f86ea8973d0c393
David Evans <dave@evans.io>
2019-05-01 14:13:24 +0100

    Remove unneeded function to create prescribing tables
    This endpoint is powered by the MatrixStore now and so doesn't use these
    tables.

--- openprescribing/frontend/tests/test_api_spending.py
+++ openprescribing/frontend/tests/test_api_spending.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 import csv
-import datetime
 import json
 
 from django.db import connection
@@ -8,7 +7,6 @@ from django.test import TestCase
 
 from .api_test_base import ApiTestBase
 
-from frontend.models import ImportLog
 from frontend.models import Prescription
 from frontend.tests.data_factory import DataFactory
 from api.views_spending import MIN_GHOST_GENERIC_DELTA
@@ -17,20 +15,6 @@ from dmd.models import DMDVmpp
 from dmd.models import TariffPrice
 
 import numpy as np
-
-
-def _create_prescribing_tables():
-    latest_date = ImportLog.objects.latest('current_at').current_at
-    cmd = ("DROP TABLE IF EXISTS %s; "
-           "CREATE TABLE %s () INHERITS (frontend_prescription)")
-    with connection.cursor() as cursor:
-        for offset in range(-59, 1):
-            year = latest_date.year + int(offset / 12)
-            month = (((latest_date.month - 1) + offset) % 12) + 1
-            date = datetime.date(year, month, 1)
-            table_name = "frontend_prescription_%s%s" % (
-                date.year, str(date.month).zfill(2))
-            cursor.execute(cmd % (table_name, table_name))
 
 
 class TestAPISpendingViewsTariff(ApiTestBase):
@@ -114,7 +98,6 @@ class TestSpending(ApiTestBase):
         self.assertEqual(response.status_code, 404)
 
     def test_total_spending(self):
-        _create_prescribing_tables()
         rows = self._get_rows({})
 
         self.assertEqual(len(rows), 60)
@@ -132,7 +115,6 @@ class TestSpending(ApiTestBase):
         self.assertEqual(rows[44]['quantity'], '5143')
 
     def test_total_spending_by_bnf_section(self):
-        _create_prescribing_tables()
         rows = self._get_rows({
             'code': '2'
         })
@@ -147,7 +129,6 @@ class TestSpending(ApiTestBase):
         self.assertEqual(rows[44]['quantity'], '5143')
 
     def test_total_spending_by_bnf_section_full_code(self):
-        _create_prescribing_tables()
         rows = self._get_rows({
             'code': '02',
         })
@@ -162,7 +143,6 @@ class TestSpending(ApiTestBase):
         self.assertEqual(rows[44]['quantity'], '5143')
 
     def test_total_spending_by_code(self):
-        _create_prescribing_tables()
         rows = self._get_rows({
             'code': '0204000I0',
         })
@@ -173,7 +153,6 @@ class TestSpending(ApiTestBase):
         self.assertEqual(rows[44]['quantity'], '2355')
 
     def test_total_spending_by_codes(self):
-        _create_prescribing_tables()
         rows = self._get_rows({
             'code': '0204000I0,0202010B0',
         })
