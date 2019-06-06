b29599f92808b9929f20f7f35b55a23308ec5c3d
Peter Inglesby <peter.inglesby@gmail.com>
2019-06-06 11:06:32 +0100

    Load MEASURE_TAGS lazily

--- openprescribing/frontend/measure_tags.py
+++ openprescribing/frontend/measure_tags.py
@@ -1,20 +1,31 @@
 import json
 import os.path
+from functools import partial
+
+from django.conf import settings
+from django.utils.functional import SimpleLazyObject
 
 from frontend.models import Measure
 
 
-def _load_measure_tags(filename):
-    with open(filename) as f:
+def _load_measure_tags(path):
+    with open(path) as f:
         tags = json.load(f)
     for tag_name, tag_details in tags.items():
         if isinstance(tag_details.get('description'), list):
             tag_details['description'] = ' '.join(
                 line.strip() for line in tag_details['description']
             )
-        tag_details['count'] = Measure.objects.filter(tags__contains=[tag_name]).count()
+        tag_details['count'] = (
+            Measure.objects.filter(tags__contains=[tag_name]).count()
+        )
     return tags
 
 
-MEASURE_TAGS = _load_measure_tags(
-    os.path.join(os.path.dirname(__file__), '../common/measure_tags.json'))
+def _lazy_load_measure_tags(path):
+    return SimpleLazyObject(partial(_load_measure_tags, path))
+
+
+MEASURE_TAGS = _lazy_load_measure_tags(
+    os.path.join(settings.APPS_ROOT, 'common', 'measure_tags.json')
+)
--- openprescribing/frontend/tests/test_measure_tags.py
+++ openprescribing/frontend/tests/test_measure_tags.py
@@ -2,23 +2,23 @@ from django.test import TestCase
 
 from frontend.tests.data_factory import DataFactory
 
-from frontend.measure_tags import _load_measure_tags
+from frontend.measure_tags import _lazy_load_measure_tags
 
 
 class TestMeasureTags(TestCase):
+    path = 'frontend/tests/fixtures/measure_tags.json'
+
     def test_counts(self):
         factory = DataFactory()
         factory.create_measure(tags=['tag1'])
         factory.create_measure(tags=['tag1', 'tag2'])
 
-        path = 'frontend/tests/fixtures/measure_tags.json'
-        tags = _load_measure_tags(path)
+        tags = _lazy_load_measure_tags(self.path)
         self.assertEqual(tags['tag1']['count'], 2)
         self.assertEqual(tags['tag2']['count'], 1)
 
     def test_descriptions(self):
-        path = 'frontend/tests/fixtures/measure_tags.json'
-        tags = _load_measure_tags(path)
+        tags = _lazy_load_measure_tags(self.path)
         self.assertEqual(
             tags['tag1']['description'],
             'This description is a string'
