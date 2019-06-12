c5345800ba1aab97cb45a87e35afca79e6547828
David Evans <dave.evans@thedatalab.org>
2019-06-12 18:26:52 +0100

    Refactor org_location endpoint

--- openprescribing/api/views_org_location.py
+++ openprescribing/api/views_org_location.py
@@ -1,8 +1,10 @@
-from rest_framework.decorators import api_view
-from frontend.models import PCT, Practice
-import view_utils as utils
 from django.http import HttpResponse
 from django.core.serializers import serialize
+
+from rest_framework.decorators import api_view
+
+from frontend.models import PCT, Practice
+import api.view_utils as utils
 
 
 @api_view(['GET'])
@@ -11,23 +13,31 @@ def org_location(request, format=None):
     centroids = request.GET.get('centroids', '')
     org_codes = utils.param_to_list(request.GET.get('q', ''))
     if org_type == 'practice':
-        org_codes = utils.get_practice_ids_from_org(org_codes)
-    if org_type == 'ccg':
-        results = PCT.objects.filter(close_date__isnull=True) \
-                             .filter(org_type='CCG')
-        if org_codes:
-            results = results.filter(code__in=org_codes)
-        if centroids:
-            fields = ('name', 'code', 'ons_code', 'org_type', 'centroid', )
-            geo_field = 'centroid'
-        else:
-            fields = ('name', 'code', 'ons_code', 'org_type', 'boundary', )
-            geo_field = 'boundary'
-
+        result_spec = _get_practices(org_codes, centroids)
+    elif org_type == 'ccg':
+        result_spec = _get_ccgs(org_codes, centroids)
     else:
-        results = Practice.objects.filter(code__in=org_codes)
-        geo_field = 'location'
-        fields = ('name', 'code', 'setting', 'is_dispensing', 'location', )
-    return HttpResponse(serialize('geojson', results,
-                                  geometry_field=geo_field, fields=fields),
-                        content_type='application/json')
+        raise ValueError('Unknown org_type: {}'.format(org_type))
+    results, geo_field, other_fields = result_spec
+    fields = other_fields + (geo_field,)
+    return HttpResponse(
+        serialize('geojson', results, geometry_field=geo_field, fields=fields),
+        content_type='application/json'
+    )
+
+
+def _get_practices(org_codes, centroids):
+    org_codes = utils.get_practice_ids_from_org(org_codes)
+    results = Practice.objects.filter(code__in=org_codes)
+    geo_field = 'location'
+    other_fields = ('name', 'code', 'setting', 'is_dispensing')
+    return results, geo_field, other_fields
+
+
+def _get_ccgs(org_codes, centroids):
+    results = PCT.objects.filter(close_date__isnull=True, org_type='CCG')
+    if org_codes:
+        results = results.filter(code__in=org_codes)
+    geo_field = 'centroid' if centroids else 'boundary'
+    other_fields = ('name', 'code', 'ons_code', 'org_type')
+    return results, geo_field, other_fields
