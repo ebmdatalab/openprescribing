ebmdatalab/openprescribing

commit 824c48b63d80d9ed1574af25aee293c25ff0e17b

Update import_stps to import from new spreadsheet

openprescribing/frontend/models.py
@@ -91,7 +91,11 @@ class RegionalTeam(models.Model):
 
 class STP(models.Model):
     ons_code = models.CharField(max_length=9, primary_key=True)
+    code = models.CharField(max_length=3, null=True)  # TODO make this the PK
     name = models.CharField(max_length=200, null=True, blank=True)
+    regional_team = models.ForeignKey(
+        RegionalTeam, null=True, on_delete=models.PROTECT
+    )
 
     def __str__(self):
         return self.name
@@ -99,10 +103,6 @@ class STP(models.Model):
     @property
     def cased_name(self):
         return nhs_titlecase(self.name)
-
-    @property
-    def code(self):
-        return self.ons_code
 
 
 class PCT(models.Model):

openprescribing/frontend/migrations/0051_auto_20190627_1536.py
@@ -1,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.11.20 on 2019-06-27 14:36
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('frontend', '0050_auto_20190627_1029'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='stp',
+            name='code',
+            field=models.CharField(max_length=3, null=True),
+        ),
+        migrations.AddField(
+            model_name='stp',
+            name='regional_team',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.PROTECT, to='frontend.RegionalTeam'),
+        ),
+    ]

openprescribing/pipeline/management/commands/import_stps.py
@@ -1,6 +1,11 @@
 import csv
+import re
+
 from django.core.management import BaseCommand
-from frontend.models import PCT, STP
+from django.db import transaction
+from frontend.models import PCT, RegionalTeam, STP
+
+from openpyxl import load_workbook
 
 
 class Command(BaseCommand):
@@ -8,12 +13,40 @@ class Command(BaseCommand):
         parser.add_argument('--filename')
 
     def handle(self, *args, **kwargs):
-        with open(kwargs['filename']) as f:
-            for row in csv.DictReader(f):
-                stp, _ = STP.objects.get_or_create(ons_code=row['STP18CD'])
-                stp.name = row['STP18NM']
+        wb = load_workbook(kwargs['filename'])
+        sheet = wb.get_sheet_by_name('Current STPs')
+
+        assert sheet['A5'].value == 'NHS England Region', sheet['A5'].value
+        assert sheet['B5'].value == 'ODS STP Code', sheet['B5'].value
+        assert sheet['D5'].value == 'ONS STP Code', sheet['D5'].value
+        assert sheet['E5'].value == 'STP Name', sheet['E5'].value
+        assert sheet['J5'].value == 'CCG', sheet['J5'].value
+        assert sheet['K5'].value == 'ODS CCG Code', sheet['K5'].value
+
+        data = [[cell.value for cell in row] for row in sheet.rows]
+
+        with transaction.atomic():
+            for row in data[5:]:
+                if not row[10]:
+                    continue
+
+                rt_code = re.search('\((\w{3})\)', row[0]).groups()[0]
+                rt = RegionalTeam.objects.get(code=rt_code)
+
+                stp_code = row[1]
+                stp_ons_code = row[3]
+                stp_name = row[4].strip()
+                stp, _ = STP.objects.get_or_create(ons_code=stp_ons_code)
+                stp.code = stp_code
+                if stp_name:
+                    # The stp_name is only present in the first row that an STP appears!
+                    stp.name = stp_name
+                stp.regional_team = rt
                 stp.save()
 
-                ccg = PCT.objects.get(code=row['CCG18CDH'])
+                ccg_code = row[10]
+                ccg = PCT.objects.get(code=ccg_code)
+                assert ccg.regional_team == rt
+                print ccg.name, 'is in STP', stp.name
                 ccg.stp = stp
                 ccg.save()

openprescribing/pipeline/tests/test_import_stps.py
@@ -1,40 +1,0 @@
-import os
-from django.conf import settings
-from django.core.management import call_command
-from django.test import TestCase
-from frontend.models import PCT, STP
-
-
-class TestImportSTPs(TestCase):
-    def test_import_stps(self):
-        # These CCGs are all referred to in stps.csv
-        for code in ['02N', '02W', '02Y', '03F', '00T', '00V']:
-            PCT.objects.create(code=code, name='CCG ' + code, org_type='CCG')
-
-        # This STP will be unchanged
-        STP.objects.create(ons_code='E54000005', name='West Yorkshire')
-
-        # This STP's name will be updated
-        STP.objects.create(ons_code='E54000006', name='Humber')
-
-        path = os.path.join(settings.APPS_ROOT, 'pipeline', 'test-data', 'stps.csv')
-        call_command('import_stps', '--filename', path)
-
-        for ons_code, name in [
-            ['E54000005', 'West Yorkshire'],
-            ['E54000006', 'Humber, Coast and Vale'],
-            ['E54000007', 'Greater Manchester'],
-        ]:
-            stp = STP.objects.get(ons_code=ons_code)
-            self.assertEqual(stp.name, name)
-
-        for ccg_code, stp_ons_code in [
-            ['02N', 'E54000005'],
-            ['02W', 'E54000005'],
-            ['02Y', 'E54000006'],
-            ['03F', 'E54000006'],
-            ['00T', 'E54000007'],
-            ['00V', 'E54000007'],
-        ]:
-            ccg = PCT.objects.get(code=ccg_code)
-            self.assertEqual(ccg.stp.ons_code, stp_ons_code)

commit 7f15249bd3946fef10d3fdfab2eb25e5e1798269

Quick fix for broken homepage
Reverts a change prematurely introduced in 824c48b63d80d9ed1574af25aee293c25ff0e17b

openprescribing/frontend/models.py
@@ -104,6 +104,10 @@ class STP(models.Model):
     def cased_name(self):
         return nhs_titlecase(self.name)
 
+    @property
+    def code(self):
+        return self.ons_code
+
 
 class PCT(models.Model):
     '''