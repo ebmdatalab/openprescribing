10dc446d57ced585679c1595811a5aa486d0d9ba
David Evans <dave.evans@thedatalab.org>
2019-07-30 15:45:49 +0100

    Rename create_views to refresh_views
    This is now a more accurate name. We also add a minimal smoketest to
    check that we can run it without blowing up.

---
+++ openprescribing/frontend/tests/commands/test_refresh_views.py
@@ -0,0 +1,9 @@
+from django.core.management import call_command
+from django.test import TestCase
+
+
+class RefreshViewsTestCase(TestCase):
+
+    def test_basic_smoketest(self):
+        # Test we can run it without it blowing up
+        call_command('refresh_views')
