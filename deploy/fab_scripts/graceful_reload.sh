8cb122954dc57ddbaa313d07b715c982f2a8b9b7
David Evans <dave.evans@thedatalab.org>
2019-08-07 16:46:36 +0100

    Support being run under both supervisor and systemd
    This PR modifies gunicorn-related scripts so that they can run under
    either supervisor (which they do at present) or systemd. This adds
    complexity temporarily, but makes the process of updating our server
    configuration much easier.
    
    The primary motivation for this is to make use of systemd's socket
    management which allows us to do application restarts which are both
    graceful (i.e. involve no dropped connections or error responses) and
    full (i.e. involve starting a completely new process instead of trying
    to load new code into an existing process). Gunicorn's graceful restarts
    are not full, and this resulted in many frustrating issues.

--- deploy/fab_scripts/graceful_reload.sh
+++ deploy/fab_scripts/graceful_reload.sh
@@ -3,12 +3,59 @@
 set -e
 
 script_dir="$( unset CDPATH && cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pid_file="$script_dir/../../run/gunicorn.pid"
 
-PID=$(cat "$pid_file")
-if [[ -n "$PID" ]]; then
-    kill -HUP $PID;
+case "$script_dir" in
+  /webapps/openprescribing/*)
+    app_name=openprescribing
+  ;;
+  /webapps/openprescribing_staging/*)
+    app_name=openprescribing_staging
+  ;;
+  *)
+    echo "Error: can't determine which environment script is running in"
+    exit 1
+  ;;
+esac
+
+# Service names follow the pattern:
+#
+#   app.<APP_NAME>.<PROCESS_NAME>.service
+#
+# The "app" prefix serves to namespace our services off from any other system services.
+#
+# APP_NAME is the name of this particular deployment of an application. If a
+# single project (e.g. openprescribing) is deployed in multiple environments
+# (e.g. production, staging) then each environment gets its own app name.
+#
+# A single application may have multiple processes and hence multiple services
+# wtih different PROCESS_NAMEs.  By convention (established by Heroku) the
+# process that runs the web application is called "web".
+#
+# The "service" suffix comes from systemd. Services which want systemd to
+# manage a socket for them (to allow for graceful restarts) will also have a
+# corresponding ".socket" configuration file to go with the ".service" file.
+#
+# Because many systemd commands allow the use of wildcards, this dotted-name
+# convention makes it easy to do things like "restart all services for this
+# app".
+service_name="app.$app_name.web.service"
+
+service_enabled=false
+if systemctl list-unit-files --type service --state enabled | grep -Fq "$service_name"; then
+  service_enabled=true
+fi
+
+# Vary the restart behaviour depending on whether or not the corresponding
+# systemd service is enabled
+if $service_enabled; then
+  systemctl restart "$service_name"
 else
-    echo "Error: server $1 not running, so could not reload";
-    exit 1;
+  pid_file="$script_dir/../../run/gunicorn.pid"
+  PID=$(cat "$pid_file")
+  if [[ -n "$PID" ]]; then
+      kill -HUP $PID;
+  else
+      echo "Error: server $1 not running, so could not reload";
+      exit 1;
+  fi
 fi
