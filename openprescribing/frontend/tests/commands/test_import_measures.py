4f938e3ea8faddcabc19a0511ac00bbcaf8629b2
Peter Inglesby <peter.inglesby@gmail.com>
2019-09-11 12:16:55 +0100

    Move last tests from test_import_measures to test_import_measures_new

--- openprescribing/frontend/tests/commands/test_import_measures.py
+++ openprescribing/frontend/tests/commands/test_import_measures.py
@@ -23,34 +23,3 @@ from frontend.models import RegionalTeam
 
 
 MODULE = "frontend.management.commands.import_measures"
-
-
-class UnitTests(TestCase):
-    """Unit tests with mocked bigquery. Many of the functional
-    tests could be moved hree.
-
-    """
-
-    fixtures = ["measures"]
-
-    @patch("common.utils.db")
-    def test_reconstructor_not_called_when_measures_specified(self, db):
-        from frontend.management.commands.import_measures import (
-            conditional_constraint_and_index_reconstructor,
-        )
-
-        with conditional_constraint_and_index_reconstructor({"measure": "thingy"}):
-            pass
-        execute = db.connection.cursor.return_value.__enter__.return_value.execute
-        execute.assert_not_called()
-
-    @patch("common.utils.db")
-    def test_reconstructor_called_when_no_measures_specified(self, db):
-        from frontend.management.commands.import_measures import (
-            conditional_constraint_and_index_reconstructor,
-        )
-
-        with conditional_constraint_and_index_reconstructor({"measure": None}):
-            pass
-        execute = db.connection.cursor.return_value.__enter__.return_value.execute
-        execute.assert_called()
