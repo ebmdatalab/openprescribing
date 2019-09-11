1b5e3ef6d86b408e3e9c5a95c3324705d8be344b
Peter Inglesby <peter.inglesby@gmail.com>
2019-09-11 12:42:47 +0100

    Add simplify_bnf_codes

---
+++ openprescribing/frontend/tests/test_bnf_hierarchy.py
@@ -0,0 +1,71 @@
+import random
+from itertools import product
+
+from django.test import TestCase
+
+from matrixstore.tests.contextmanagers import (
+    patched_global_matrixstore_from_data_factory,
+)
+from matrixstore.tests.data_factory import DataFactory
+
+from frontend.utils.bnf_hierarchy import simplify_bnf_codes, _prune_paths
+
+
+class TestPrunePaths(TestCase):
+    def test_example(self):
+        # Test the example described in the docstring of _prune_paths.
+
+        all_paths = ["AAA", "AAB", "ABA", "ABB", "BAA", "BAB", "BBA", "BBB"]
+        paths = ["AAA", "ABA", "ABB", "BAA", "BAB"]
+        self.assertEqual(_prune_paths(paths, all_paths), ["AAA", "AB", "BA"])
+
+    def test_full(self):
+        # With a set of paths built from a complete binary tree of height 6, choose a
+        # subset of paths of each length between 1 and 63, and check that _prune_paths
+        # does not raise an AssertionError for that subset.
+
+        random.seed(123)
+        all_paths = ["".join(tpl) for tpl in product(*["AB" for _ in range(6)])]
+
+        for size in range(1, 2 ** 6):
+            paths = random.sample(all_paths, size)
+            _prune_paths(paths, all_paths)
+
+
+class TestSimplifyBnfCodes(TestCase):
+    def test_simplify_bnf_codes(self):
+        # These are all BNF codes for Aluminium Hydroxide.
+        all_bnf_codes = [
+            "0101010C0AAAAAA",
+            "0101010C0AAACAC",
+            "0101010C0AAADAD",
+            "0101010C0AAAFAF",
+            "0101010C0AAAGAG",
+            "0101010C0AAAHAH",
+            "0101010C0AAAIAI",
+            "0101010C0AAAJAJ",
+            "0101010C0AAAKAK",
+            "0101010C0AAALAL",
+            "0101010C0AAAMAM",
+            "0101010C0BBAAAA",
+            "0101010C0BCAAAH",
+            "0101010C0BDAAAC",
+            "0101010C0BDABAC",
+        ]
+
+        factory = DataFactory()
+        month = factory.create_months("2018-10-01", 1)[0]
+        practice = factory.create_practices(1)[0]
+        for bnf_code in all_bnf_codes:
+            presentation = factory.create_presentation(bnf_code)
+            factory.create_prescription(presentation, practice, month)
+
+        branded_bnf_codes = [
+            "0101010C0BBAAAA",
+            "0101010C0BCAAAH",
+            "0101010C0BDAAAC",
+            "0101010C0BDABAC",
+        ]
+
+        with patched_global_matrixstore_from_data_factory(factory):
+            self.assertEqual(simplify_bnf_codes(branded_bnf_codes), ["0101010C0B"])
---
+++ openprescribing/frontend/utils/bnf_hierarchy.py
@@ -0,0 +1,208 @@
+from matrixstore.db import get_db
+
+
+def simplify_bnf_codes(bnf_codes):
+    """Given list of BNF codes, return list of BNF prefixes such that:
+
+        1. every BNF code that belongs to one of these prefixes is in the original list,
+        2. every code in the original list belongs to exactly one prefix,
+        3. no prefix is a prefix of another prefix (I think this follows from 2).
+
+    A BNF prefix may actually be a full BNF code.
+    """
+
+    all_bnf_codes = get_all_bnf_codes()
+    return _prune_paths(bnf_codes, all_bnf_codes)
+
+
+def get_all_bnf_codes():
+    """Return list of all BNF codes for which we have prescribing."""
+
+    db = get_db()
+    return [r[0] for r in db.query("SELECT bnf_code FROM presentation")]
+
+
+def _prune_paths(paths, all_paths):
+    """Given two lists of paths (`paths` and `all_paths`) from the root of a tree to its
+    leaves (where every path in `paths` is in `all_paths`) return a new list of
+    paths from the root of the tree to either a leaf or a branch (`pruned_paths`) such
+    that:
+
+        1. every path in `all_paths` that is reachable from a path in `pruned_paths` is
+            in `paths`,
+        2. every path in `paths` is reachable from exactly one path in `pruned_paths`,
+        3. no path in `pruned_paths` is reachable from another path in `pruned_paths`.
+
+    These three conditions correspond to those in the docstring for simplify_bnf_codes.
+
+    For instance:
+
+        all_paths: [AAA, AAB, ABA, ABB, BAA, BAB, BBA, BBB]
+        paths:     [AAA,      ABA, ABB, BAA, BAB,         ]
+
+    To do this, we convert `paths` and `all_paths` into trees:
+
+        all_paths:                  paths:
+
+                   A                           A
+                  /                           /
+                 A                           A
+                / \                         /
+               /   B                       /
+              A                           A
+             / \   A                     / \   A
+            /   \ /                     /   \ /
+           /     B                     /     B
+          /       \                   /       \
+         /         B                 /         B
+        o                           o
+         \         A                 \         A
+          \       /                   \       /
+           \     A                     \     A
+            \   / \                     \   / \
+             \ /   B                     \ /   B
+              B                           B
+               \   A
+                \ /
+                 B
+                  \
+                   B
+
+    We then prune the `paths` tree, where for each node, we remove all of the node's
+    children if all of the children are in the `all_paths` tree.
+
+    So we remove the children of AB (since ABA and ABB are in `paths`) and BA (since BAA
+    and BAB are in `paths`).
+
+    This leaves:
+
+                   A
+                  /
+                 A
+                /
+               /
+              A
+             / \
+            /   \
+           /     B
+          /
+         /
+        o
+         \
+          \
+           \     A
+            \   /
+             \ /
+              B
+
+    Finally, we walk this pruned tree to give:
+
+    pruned_paths: [AAA, AB, BA]
+
+    A tree is representated as a nested dictionary.  For instance, the `paths` tree is
+    represented as:
+
+    {
+        "A": {
+            "A": {
+                "A": {},
+            },
+            "B": {
+                "A": {},
+                "B": {}
+            }
+        },
+        "B": {
+            "A": {
+                "A": {},
+                "B": {}
+            }
+        },
+    }
+
+    And the `pruned_paths` tree:
+
+    {
+        "A": {
+            "A": {
+                "A": {}
+            },
+            "B": {}
+        },
+        "B": {
+            "A": {}
+        }
+    }
+
+    There is a test case for this example at TestPrunePaths.test_example.
+    """
+
+    assert set(paths) < set(all_paths)  # `paths` must be a subset of `all_paths`
+
+    full_tree = _paths_to_tree(all_paths)
+    subtree = _paths_to_tree(paths)
+    pruned_subtree = _prune_tree(subtree, full_tree)
+    pruned_paths = _tree_to_paths(pruned_subtree)
+
+    # We now verify the three conditions in the docstring hold.
+
+    # 1. every path in `all_paths` that is reachable from a path in `pruned_paths` is in
+    #     `paths`.
+    for path in all_paths:
+        if any(path.startswith(pruned_path) for pruned_path in pruned_paths):
+            assert path in paths
+
+    # 2. every path in `paths` is reachable from exactly one path in `pruned_paths`.
+    for path in paths:
+        # print(path)
+        parent_pruned_paths = [
+            pruned_path for pruned_path in pruned_paths if path.startswith(pruned_path)
+        ]
+        assert len(parent_pruned_paths) == 1
+
+    # 3. no path in `pruned_paths` is reachable from another path in `pruned_paths`.
+    for pruned_path_1 in pruned_paths:
+        for pruned_path_2 in pruned_paths:
+            if pruned_path_1 != pruned_path_2:
+                assert not pruned_path_1.startswith(pruned_path_2)
+
+    return pruned_paths
+
+
+def _prune_tree(subtree, tree):
+    """Do the work.
+
+    I'm struggling to write a good docstring here.
+    """
+
+    if subtree == tree:
+        return {}
+    return {c: _prune_tree(subtree[c], tree[c]) for c in subtree}
+
+
+def _paths_to_tree(paths):
+    """Build a tree from the given paths.
+
+    The structure of the tree is described in the docstring for _prune_paths.
+    """
+
+    tree = {}
+    for path in paths:
+        t = tree
+        for c in path:
+            if c not in t:
+                t[c] = {}
+            t = t[c]
+    return tree
+
+
+def _tree_to_paths(tree):
+    """Build a list of paths made by walking from the root of the tree to each leaf."""
+
+    if len(tree) == 0:
+        return [""]
+    return [
+        c + path
+        for c, subtree in sorted(tree.items())
+        for path in _tree_to_paths(subtree)
+    ]
