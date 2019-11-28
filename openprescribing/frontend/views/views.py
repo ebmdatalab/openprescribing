ae5596500377400151202c8c23a60bb84bd4dc4e
Peter Inglesby <peter.inglesby@gmail.com>
2019-11-28 13:12:27 +0000

    Move bookmark_views into views.py

--- openprescribing/frontend/views/views.py
+++ openprescribing/frontend/views/views.py
@@ -32,6 +32,7 @@ from common.utils import parse_date
 from api.view_utils import dictfetchall
 from common.utils import ppu_sql
 from dmd.models import VMP
+from frontend.forms import BookmarkListForm
 from frontend.forms import FeedbackForm
 from frontend.forms import OrgBookmarkForm
 from frontend.forms import SearchBookmarkForm
@@ -47,6 +48,7 @@ from frontend.models import NCSOConcessionBookmark
 from frontend.models import Practice, PCT, Section
 from frontend.models import PCN
 from frontend.models import Presentation
+from frontend.models import Profile
 from frontend.models import RegionalTeam
 from frontend.models import STP
 from frontend.models import SearchBookmark
@@ -56,6 +58,7 @@ from frontend.views.spending_utils import (
     ncso_spending_breakdown_for_entity,
     NATIONAL_AVERAGE_DISCOUNT_PERCENTAGE,
 )
+
 
 logger = logging.getLogger(__name__)
 
@@ -983,6 +986,75 @@ def _financial_ytd_total(monthly_totals):
 ##################################################
 # Bookmarks.
 ##################################################
+
+
+def bookmarks(request, key):
+    profile = get_object_or_404(Profile, key=key)
+    user = profile.user
+
+    search_bookmarks = user.searchbookmark_set.all()
+    org_bookmarks = user.orgbookmark_set.all()
+    ncso_concessions_bookmarks = user.ncsoconcessionbookmark_set.all()
+
+    if request.method == "POST":
+        if not request.POST.get("unsuball"):
+            org_bookmarks = org_bookmarks.filter(
+                pk__in=request.POST.getlist("org_bookmarks")
+            )
+            search_bookmarks = search_bookmarks.filter(
+                pk__in=request.POST.getlist("search_bookmarks")
+            )
+            ncso_concessions_bookmarks = ncso_concessions_bookmarks.filter(
+                pk__in=request.POST.getlist("ncso_concessions_bookmarks")
+            )
+
+        # QuerySet.delete() returns a tuple whose first element is the number
+        # of records deleted.
+        count = (
+            org_bookmarks.delete()[0]
+            + search_bookmarks.delete()[0]
+            + ncso_concessions_bookmarks.delete()[0]
+        )
+
+        if count > 0:
+            msg = "Unsubscribed from %s alert" % count
+            if count > 1:
+                msg += "s"
+            messages.success(request, msg)
+
+        return redirect(reverse("bookmarks", args=[key]))
+
+    form = BookmarkListForm(
+        org_bookmarks=org_bookmarks,
+        search_bookmarks=search_bookmarks,
+        ncso_concessions_bookmarks=ncso_concessions_bookmarks,
+    )
+    count = (
+        search_bookmarks.count()
+        + org_bookmarks.count()
+        + ncso_concessions_bookmarks.count()
+    )
+
+    if count == 1:
+        single_bookmark = (
+            search_bookmarks.first()
+            or org_bookmarks.first()
+            or ncso_concessions_bookmarks.first()
+        )
+    else:
+        single_bookmark = None
+
+    ctx = {
+        "search_bookmarks": search_bookmarks,
+        "org_bookmarks": org_bookmarks,
+        "ncso_concessions_bookmarks": ncso_concessions_bookmarks,
+        "form": form,
+        "count": count,
+        "single_bookmark": single_bookmark,
+    }
+
+    return render(request, "bookmarks/bookmark_list.html", ctx)
+
 
 BOOKMARK_CLS_TO_FORM_CLS = {
     OrgBookmark: OrgBookmarkForm,
