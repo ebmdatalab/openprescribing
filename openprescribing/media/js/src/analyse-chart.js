ebmdatalab/openprescribing

commit 7979fc495156cd18b2bc1cff6787b91fcd85f5c0

Cusum based alerts (#442)
Implement alerts using CUSUM method.

Includes making previews available for anonymous users, and Google Optimize support.

Closes #217

openprescribing/templates/_alert_signup_form.html
@@ -1,19 +1,22 @@
 {% load crispy_forms_tags %}
 {% if not signed_up_for_alert %}
 <p>
-  Get monthly alerts about prescribing at this {{entity_type}}:
+  <form method="post" action="{{ alert_preview_action }}">
+   {% csrf_token %}
+   {% if alert_type == 'analyse' %}
+     <input type="hidden" name="url" value="">
+     <input type="hidden" name="name" value="">
+     We offer a service which emails you an update of this chart once a month. Subscribe by entering your email address below.
+   {% else %}
+     We offer a service which emails you about unusual or interesting prescribing at this {{entity_type}} once a month. Subscribe by entering your email address below.
+   {% endif %}
+   You can <input type="submit" class="btn-link" value="preview"></input> the email first.
+  </form>
   <form method="post" class="form form-inline">
     {% csrf_token %}
     {{ form.non_field_errors }}
     {{ form | crispy }}
     <input class="btn btn-primary" type="submit" value="Subscribe">
-    {% if request.user.is_superuser %}
-     {% if entity_type == 'CCG' %}
-       <a href="{% url 'preview-ccg-bookmark' ccg.code %}" class="btn btn-danger">Preview alert email</a>
-     {% else %}
-       <a href="{% url 'preview-practice-bookmark' practice.code %}" class="btn btn-danger">Preview alert email</a>
-     {% endif %}
-    {% endif %}
 
   </form>
 </p>

openprescribing/api/views_spending.py
@@ -12,7 +12,7 @@ def total_spending(request, format=None):
 
     spending_type = utils.get_spending_type(codes)
     if spending_type is False:
-        err = "Error: BNF Codes must all be the same length if written in the same search box. For example, you cannot search for Cerazette_Tab 75mcg (0703021Q0BBAAAA) and Cerelle (0703021Q0BD), but you could search for Cerazette (0703021Q0BB) and Cerelle (0703021Q0BD). If you need this data, please <a href='mailto:feedback@openprescribing.net' class='doorbell-show'>get in touch</a> and we may be able to extract it for you"
+        err = 'Error: BNF Codes must all be the same length if written in the same search box. For example, you cannot search for Cerazette_Tab 75mcg (0703021Q0BBAAAA) and Cerelle (0703021Q0BD), but you could search for Cerazette (0703021Q0BB) and Cerelle (0703021Q0BD). If you need this data, please <a href="mailto:{{ SUPPORT_EMAIL }}" class="doorbell-show">get in touch</a> and we may be able to extract it for you'
         return Response(err, status=400)
 
     query = _get_query_for_total_spending(codes)
@@ -33,7 +33,7 @@ def spending_by_ccg(request, format=None):
 
     spending_type = utils.get_spending_type(codes)
     if spending_type is False:
-        err = "Error: BNF Codes must all be the same length if written in the same search box. For example, you cannot search for Cerazette_Tab 75mcg (0703021Q0BBAAAA) and Cerelle (0703021Q0BD), but you could search for Cerazette (0703021Q0BB) and Cerelle (0703021Q0BD). If you need this data, please <a href='mailto:feedback@openprescribing.net' class='doorbell-show'>get in touch</a> and we may be able to extract it for you"
+        err = 'Error: BNF Codes must all be the same length if written in the same search box. For example, you cannot search for Cerazette_Tab 75mcg (0703021Q0BBAAAA) and Cerelle (0703021Q0BD), but you could search for Cerazette (0703021Q0BB) and Cerelle (0703021Q0BD). If you need this data, please <a href="mailto:{{ SUPPORT_EMAIL }}" class="doorbell-show">get in touch</a> and we may be able to extract it for you'
         return Response(err, status=400)
 
     if not spending_type or spending_type == 'bnf-section' \

openprescribing/frontend/context_processors.py
@@ -19,6 +19,9 @@ def google_tracking_id(request):
     tracking_id = None
     if hasattr(settings, 'GOOGLE_TRACKING_ID'):
         tracking_id = settings.GOOGLE_TRACKING_ID
+        container_id = getattr(settings, 'GOOGLE_OPTIMIZE_CONTAINER_ID', '')
     else:
         logger.warn("No GOOGLE_TRACKING_ID set")
-    return {'GOOGLE_TRACKING_ID': tracking_id}
+    return {
+        'GOOGLE_TRACKING_ID': tracking_id,
+        'GOOGLE_OPTIMIZE_CONTAINER_ID': container_id}

openprescribing/frontend/management/commands/send_monthly_alerts.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import sys
+import sets
+import traceback
 
 from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
@@ -160,7 +163,18 @@ class Command(BaseCommand):
 
 class BatchedEmailErrors(Exception):
     def __init__(self, exceptions):
-        msg = ("Encountered %s mail exceptions" % len(exceptions))
+        individual_messages = sets.Set()
+        for exception in exceptions:
+            individual_messages.add(
+                "".join(traceback.format_exception_only(
+                    exception[0], exception[1])).strip())
+        if len(exceptions) > 1:
+            msg = ("Encountered %s mail exceptions "
+                   "(showing last traceback only): `%s`" % (
+                       len(exceptions),
+                       ", ".join(individual_messages)))
+        else:
+            msg = individual_messages.pop()
         super(BatchedEmailErrors, self).__init__(msg)
 
 
@@ -173,14 +187,18 @@ class EmailRetrier(object):
         try:
             callback()
         except Exception as e:
-            self.exceptions.append(e)
+            self.exceptions.append(sys.exc_info())
             logger.exception(e)
             if len(self.exceptions) > self.max_errors:
-                raise BatchedEmailErrors(self.exceptions)
+                raise (BatchedEmailErrors(self.exceptions),
+                       None, self.exceptions[-1][2])
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.exceptions:
-            raise BatchedEmailErrors(self.exceptions)
+            exception = BatchedEmailErrors(self.exceptions)
+            raise (exception,
+                   None,
+                   self.exceptions[-1][2])

openprescribing/frontend/tests/commands/test_send_monthly_alerts.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 import re
 import unittest
 
@@ -10,6 +11,7 @@ from django.core.management import call_command
 from django.core.management.base import CommandError
 from django.test import TestCase
 from frontend.models import EmailMessage
+from frontend.models import ImportLog
 from frontend.models import Measure
 from frontend.management.commands.send_monthly_alerts import Command
 from frontend.management.commands.send_monthly_alerts import BatchedEmailErrors
@@ -187,7 +189,10 @@ class OrgEmailTestCase(TestCase):
     def test_email_body_has_ga_tracking(self, attach_image, finder):
         measure = Measure.objects.get(pk='cerazette')
         call_mocked_command_with_defaults(
-            _makeContext(declines=[(measure, 99.92, 0.12, 10.002)]),
+            _makeContext(declines=[
+                {'measure': measure,
+                 'from': 99.92,
+                 'to': 0.12}]),
             finder)
         message = mail.outbox[-1].alternatives[0]
         html = message[0]
@@ -198,13 +203,16 @@ class OrgEmailTestCase(TestCase):
         attach_image.return_value = 'unique-image-id'
         measure = Measure.objects.get(pk='cerazette')
         call_mocked_command_with_defaults(
-            _makeContext(declines=[(measure, 99.92, 0.12, 10.002)]),
+            _makeContext(declines=[
+                {'measure': measure,
+                 'from': 99.92,
+                 'to': 0.12}]),
             finder)
         message = mail.outbox[-1].alternatives[0]
         html = message[0]
         self.assertIn("this practice slipped", html)
         self.assertRegexpMatches(
-            html, 'slipped massively .* on '
+            html, 'slipped massively on '
             '<a href=".*/practice/P87629/.*#cerazette".*>'
             'Cerazette vs. Desogestrel</a>')
         self.assertIn('<span class="worse"', html)
@@ -216,8 +224,12 @@ class OrgEmailTestCase(TestCase):
         measure = Measure.objects.get(pk='cerazette')
         call_mocked_command_with_defaults(
             _makeContext(declines=[
-                (measure, 99.92, 0.12, 10.002),
-                (measure, 30, 10, 0),
+                {'measure': measure,
+                 'from': 99.92,
+                 'to': 0.12},
+                {'measure': measure,
+                 'from': 30,
+                 'to': 10},
             ]),
             finder)
         message = mail.outbox[-1].alternatives[0]
@@ -229,9 +241,15 @@ class OrgEmailTestCase(TestCase):
         measure = Measure.objects.get(pk='cerazette')
         call_mocked_command_with_defaults(
             _makeContext(declines=[
-                (measure, 99.92, 0.12, 10.002),
-                (measure, 30, 10, 0),
-                (measure, 20, 10, 0)
+                {'measure': measure,
+                 'from': 99.92,
+                 'to': 0.12},
+                {'measure': measure,
+                 'from': 30,
+                 'to': 10},
+                {'measure': measure,
+                 'from': 20,
+                 'to': 10},
             ]),
             finder)
         message = mail.outbox[-1].alternatives[0]
@@ -338,7 +356,12 @@ class OrgEmailTestCase(TestCase):
 
 @patch('frontend.views.bookmark_utils.attach_image')
 class SearchEmailTestCase(TestCase):
-    fixtures = ['bookmark_alerts']
+    fixtures = ['bookmark_alerts', 'measures']
+
+    def setUp(self):
+        ImportLog.objects.create(
+            category='prescribing',
+            current_at=datetime.datetime.today())
 
     def test_all_recipients(self, attach_image):
         call_command(CMD_NAME)

openprescribing/frontend/tests/test_bookmark_utils.py
@@ -1,7 +1,9 @@
 from dateutil.relativedelta import relativedelta
 import os
+import re
 import unittest
 
+from django.test import SimpleTestCase
 from django.test import TestCase
 import base64
 from datetime import datetime
@@ -82,95 +84,87 @@ class IntroTextTest(unittest.TestCase):
                       "are some potential cost savings", msg)
 
 
-class TestRemoveJagged(unittest.TestCase):
-    def _makeSome(self, percentiles):
-        m = Measure(is_percentage=False)
-        return [MeasureValue(percentile=percentile, measure=m)
-                for percentile in percentiles]
+def each_cusum_test(test_cases):
+    """Iterate over tests defined in the input file, validating as we go.
 
-    def _makeSomeWithPercentCalcValues(self, percentiles):
-        m = Measure(is_percentage=True)
-        return [MeasureValue(measure=m,
-                             percentile=percentile,
-                             calc_value=value)
-                for percentile, value in percentiles]
+    It validates as follows:
 
-    def _makeSomeWithNumeratorValues(self, percentiles):
-        m = Measure(is_percentage=False)
-        return [MeasureValue(measure=m, percentile=percentile, numerator=value)
-                for percentile, value in percentiles]
+    One test in the input file is a comment, followed by input data,
+    followed by expected outputs (`d` or `u`), followed by a blank
+    line.
 
-    def test_percentiles_at_extremes_one_extreme_ok(self):
-        vals = [(5, 0.9), (6, 1.0), (4, 0.8)]
-        filtered = bookmark_utils.remove_jagged(
-            self._makeSomeWithPercentCalcValues(vals))
-        self.assertEqual(
-            len(filtered), len(vals))
+    Input data and expected outputs should be left aligned in columns
+    of width 4.
 
-    def test_percentiles_at_extremes_two_extremes_bad(self):
-        vals = [(5, 0.9), (6, 1.0), (4, 0.0)]
-        filtered = bookmark_utils.remove_jagged(
-            self._makeSomeWithPercentCalcValues(vals))
-        self.assertNotEqual(
-            len(filtered), len(vals))
+    """
+    for i in range(0, len(test_cases), 4):
+        # Validate each row first
+        alignment_header = '*'.join(['...'] * 12)
+        comment_msg = "At line %s: %s does not start with #" % (
+            i, test_cases[i])
+        assert test_cases[i].startswith("#"), comment_msg
+        test_name = test_cases[i].strip()
+        alignment_msg = ("%s: Every column must be three wide "
+                         "followed by a space: \n%s\n%s" %
+                         (test_name, alignment_header, test_cases[i+1]))
+        assert test_cases[i+1][3::4].strip() == '', alignment_msg
+        directions = [n for n, ltr in enumerate(test_cases[i+2])
+                      if ltr in ('u', 'd')]
+        alignment_msg = ("%s: Every column must be three wide followed "
+                         "by a space:\n%s\n%s" % (
+                               test_name,
+                               alignment_header,
+                               str(test_cases[i+2])))
+        assert sum([x % 4 for x in directions]) == 0, alignment_msg
+        data = [round(int(x)/100.0, 2) if x.strip() else None
+                for x in re.findall('(   |\d+ {0,2}) ?', test_cases[i+1])]
+        expected = test_cases[i+2].rstrip()
+        yield {'name': test_name, 'data': data, 'expected': expected}
 
-    def test_non_percentiles_extremes(self):
-        vals = [5, 5, 5, 5, 5, 5, 5, 0, 0]
-        filtered = bookmark_utils.remove_jagged(
-            self._makeSome(vals))
-        self.assertNotEqual(
-            len(filtered), len(vals))
 
-    def test_non_percentiles_no_low_numerators(self):
-        vals = [(5, 30), (6, 20), (4, 30)]
-        filtered = bookmark_utils.remove_jagged(
-            self._makeSomeWithNumeratorValues(vals))
-        self.assertEqual(
-            len(filtered), len(vals))
+def extract_percentiles_for_alerts(result):
+    neg = result['alert_percentile_neg']
+    pos = result['alert_percentile_pos']
+    combined = []
+    assert len(neg) == len(pos)
+    for i, val in enumerate(neg):
+        if val:
+            assert not pos[i]
+            combined.append('d')
+        elif pos[i]:
+            combined.append('u')
+        else:
+            combined.append(' ')
+    return "   ".join(combined).rstrip()
 
-    def test_non_percentiles_with_low_numerators(self):
-        vals = [(5, 10), (6, 12), (4, 30)]
-        filtered = bookmark_utils.remove_jagged(
-            self._makeSomeWithNumeratorValues(vals))
-        self.assertNotEqual(
-            len(filtered), len(vals))
 
-    def test_very_jagged(self):
-        vals = [1, 100, 1, 100, 1, 50, 40, 100]
-        filtered = bookmark_utils.remove_jagged(self._makeSome(vals))
-        self.assertNotEqual(
-            len(filtered), len(vals))
+class TestCUSUM(unittest.TestCase):
+    def test_various_data(self):
+        """Test alert logic against data in the specified fixture.
 
-    @unittest.skip('should be fixed by better algorithm')
-    def test_low_not_jagged(self):
-        vals = [0, 1, 0, 1, 0, 1, 0, 1]
-        filtered = bookmark_utils.remove_jagged(self._makeSome(vals))
-        self.assertEqual(
-            len(filtered), len(vals))
+        Note that the input data is actually divided by 100 before
+        being input to the function under test. This slightly
+        unfortunate design comes from wanting to exercise a couple of
+        interesting edge cases without making the tests less readable
+        by introducing extra floating points to the test input
+        fixture.
 
-    def test_low_not_jagged_not_zero(self):
-        vals = [1, 2, 1, 2, 1, 2, 1, 2]
-        filtered = bookmark_utils.remove_jagged(self._makeSome(vals))
-        self.assertEqual(
-            len(filtered), len(vals))
-
-    def test_quite_jagged(self):
-        vals = [0, 100, 0, 50, 10, 20, 100]
-        filtered = bookmark_utils.remove_jagged(self._makeSome(vals))
-        self.assertNotEqual(
-            len(filtered), len(vals))
-
-    def test_slightly_jagged(self):
-        vals = [30, 70, 50, 60, 50, 40, 10]
-        filtered = bookmark_utils.remove_jagged(self._makeSome(vals))
-        self.assertEqual(
-            len(filtered), len(vals))
-
-    def test_not_at_all_jagged(self):
-        vals = [40, 50, 60, 70, 80]
-        filtered = bookmark_utils.remove_jagged(self._makeSome(vals))
-        self.assertEqual(
-            len(filtered), len(vals))
+        """
+        with open(
+                settings.SITE_ROOT + '/frontend/tests/fixtures/'
+                'alert_test_cases.txt', 'rb') as expected:
+            test_cases = expected.readlines()
+        for test in each_cusum_test(test_cases):
+            new_result = bookmark_utils.CUSUM(
+                test['data'], window_size=3, sensitivity=5).work()
+            new_result_formatted = extract_percentiles_for_alerts(new_result)
+            error_message = "In test '%s':\n" % test['name']
+            error_message += "   Input values: %s\n" % test['data']
+            error_message += "Expected alerts: %s\n" % test['expected']
+            self.assertEqual(
+                new_result_formatted,
+                test['expected'],
+                error_message + "            Got: %s" % new_result_formatted)
 
 
 class TestBookmarkUtilsPerforming(TestCase):
@@ -222,14 +216,6 @@ class TestBookmarkUtilsPerforming(TestCase):
         worst_measures = finder.worst_performing_in_period(3)
         self.assertIn(self.measure, worst_measures)
 
-    def test_miss_where_not_better_in_specified_number_of_months(self):
-        self.measure.low_is_good = False
-        self.measure.save()
-        finder = bookmark_utils.InterestingMeasureFinder(
-            pct=self.pct)
-        worst_measures = finder.worst_performing_in_period(3)
-        self.assertFalse(worst_measures)
-
     def test_miss_where_not_enough_global_data(self):
         finder = bookmark_utils.InterestingMeasureFinder(
             pct=self.pct)
@@ -257,13 +243,47 @@ class TestBookmarkUtilsPerforming(TestCase):
         best_measures = finder.best_performing_in_period(3)
         self.assertIn(self.measure, best_measures)
 
-    def test_no_hit_where_practice_best_and_low_is_bad(self):
-        self.measure.low_is_good = False
-        self.measure.save()
-        finder = bookmark_utils.InterestingMeasureFinder(
-            practice=self.low_percentile_practice)
-        best_measures = finder.best_performing_in_period(3)
-        self.assertFalse(best_measures)
+
+class TestLastAlertFinding(SimpleTestCase):
+    def test_no_alert_when_empty(self):
+        cusum = bookmark_utils.CUSUM(['a', 'b'])
+        cusum.alert_indices = []
+        self.assertIsNone(cusum.get_last_alert_info(), None)
+
+    def test_no_alert_when_alert_not_most_recent(self):
+        cusum = bookmark_utils.CUSUM(['a', 'b'])
+        cusum.alert_indices = [0]
+        self.assertIsNone(cusum.get_last_alert_info(), None)
+
+    def test_alert_parsed_when_only_alert(self):
+        cusum = bookmark_utils.CUSUM(['a', 'b', 'c'])
+        cusum.alert_indices = [2]
+        self.assertDictEqual(
+            cusum.get_last_alert_info(),
+            {'from': 'b',
+             'to': 'c',
+             'period': 1}
+        )
+
+    def test_period_parsed(self):
+        cusum = bookmark_utils.CUSUM(['a', 'b', 'c'])
+        cusum.alert_indices = [1, 2]
+        self.assertDictEqual(
+            cusum.get_last_alert_info(),
+            {'from': 'a',
+             'to': 'c',
+             'period': 2}
+        )
+
+    def test_alert_parsed_when_more_than_one_alert(self):
+        cusum = bookmark_utils.CUSUM(['1', '2', 'a', 'b'])
+        cusum.alert_indices = [1, 3]
+        self.assertDictEqual(
+            cusum.get_last_alert_info(),
+            {'from': 'a',
+             'to': 'b',
+             'period': 1}
+        )
 
 
 class TestBookmarkUtilsChanging(TestCase):
@@ -302,75 +322,46 @@ class TestBookmarkUtilsChanging(TestCase):
         self.practice_with_high_change = practice_with_high_change
         self.practice_with_high_neg_change = practice_with_high_neg_change
 
-    def test_low_change_not_returned_for_practice(self):
-        finder = bookmark_utils.InterestingMeasureFinder(
-            practice=self.practice_with_low_change,
-            interesting_percentile_change=10
-        )
-        self.assertEqual(finder.most_change_in_period(3),
-                         {'improvements': [],
-                          'declines': []})
-
-    def test_low_change_not_returned_for_ccg(self):
-        # This test will raise a warning due to all imput being
-        # None. Silence it.
-        import warnings
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", RuntimeWarning)
-            finder = bookmark_utils.InterestingMeasureFinder(
-                pct=self.practice_with_low_change.ccg,
-                interesting_percentile_change=10
-            )
-            self.assertEqual(finder.most_change_in_period(3),
-                             {'improvements': [],
-                              'declines': []})
-
     def test_high_change_returned(self):
         finder = bookmark_utils.InterestingMeasureFinder(
             practice=self.practice_with_high_change,
-            interesting_percentile_change=10)
-        sorted_measure = finder.most_change_in_period(3)
+            interesting_change_window=10)
+        sorted_measure = finder.most_change_against_window(1)
         measure_info = sorted_measure['improvements'][0]
         self.assertEqual(
-            measure_info[0].id, 'cerazette')
+            measure_info['measure'].id, 'cerazette')
         self.assertAlmostEqual(
-            measure_info[1], 7)   # start
+            measure_info['from'], 7)   # start
         self.assertAlmostEqual(
-            measure_info[2], 21)  # end
-        self.assertAlmostEqual(
-            measure_info[3], 0)   # residuals
+            measure_info['to'], 21)  # end
 
     def test_high_change_declines_when_low_is_good(self):
         self.measure.low_is_good = True
         self.measure.save()
         finder = bookmark_utils.InterestingMeasureFinder(
             practice=self.practice_with_high_change,
-            interesting_percentile_change=10)
-        sorted_measure = finder.most_change_in_period(3)
+            interesting_change_window=10)
+        sorted_measure = finder.most_change_against_window(1)
         measure_info = sorted_measure['declines'][0]
         self.assertEqual(
-            measure_info[0].id, 'cerazette')
+            measure_info['measure'].id, 'cerazette')
         self.assertAlmostEqual(
-            measure_info[1], 7)   # start
+            measure_info['from'], 7)   # start
         self.assertAlmostEqual(
-            measure_info[2], 21)  # end
-        self.assertAlmostEqual(
-            measure_info[3], 0)   # residuals
+            measure_info['to'], 21)  # end
 
     def test_high_negative_change_returned(self):
         finder = bookmark_utils.InterestingMeasureFinder(
             practice=self.practice_with_high_neg_change,
-            interesting_percentile_change=10)
-        sorted_measure = finder.most_change_in_period(3)
+            interesting_change_window=10)
+        sorted_measure = finder.most_change_against_window(1)
         measure_info = sorted_measure['declines'][0]
         self.assertEqual(
-            measure_info[0].id, 'cerazette')
+            measure_info['measure'].id, 'cerazette')
         self.assertAlmostEqual(
-            measure_info[1], 21)  # start
+            measure_info['from'], 21)  # start
         self.assertAlmostEqual(
-            measure_info[2], 7)   # end
-        self.assertAlmostEqual(
-            measure_info[3], 0)   # residuals
+            measure_info['to'], 7)   # end
 
 
 def _makeCostSavingMeasureValues(measure, practice, savings):
@@ -590,24 +581,31 @@ class UnescapeTestCase(unittest.TestCase):
 
 
 class TestContextForOrgEmail(unittest.TestCase):
+    def setUp(self):
+        ImportLog.objects.create(
+            category='prescribing',
+            current_at=datetime.today())
+
     @patch('frontend.views.bookmark_utils.InterestingMeasureFinder.'
            'worst_performing_in_period')
     @patch('frontend.views.bookmark_utils.InterestingMeasureFinder.'
            'best_performing_in_period')
     @patch('frontend.views.bookmark_utils.InterestingMeasureFinder.'
-           'most_change_in_period')
+           'most_change_against_window')
     def test_non_ordinal_sorting(
             self,
-            most_change_in_period,
+            most_change_against_window,
             best_performing_in_period,
             worst_performing_in_period):
         ordinal_measure_1 = MagicMock(low_is_good=True)
         non_ordinal_measure_1 = MagicMock(low_is_good=None)
         non_ordinal_measure_2 = MagicMock(low_is_good=None)
-        most_change_in_period.return_value = {
+        most_change_against_window.return_value = {
             'improvements': [
-                (ordinal_measure_1,)],
-            'declines': [(non_ordinal_measure_1,), (non_ordinal_measure_2,)]
+                {'measure': ordinal_measure_1}],
+            'declines': [
+                {'measure': non_ordinal_measure_1},
+                {'measure': non_ordinal_measure_2}]
         }
         best_performing_in_period.return_value = [
             ordinal_measure_1, non_ordinal_measure_1]
@@ -618,7 +616,9 @@ class TestContextForOrgEmail(unittest.TestCase):
         context = finder.context_for_org_email()
         self.assertEqual(
             context['most_changing_interesting'],
-            [(non_ordinal_measure_1,), (non_ordinal_measure_2,)])
+            [{'measure': non_ordinal_measure_1},
+             {'measure': non_ordinal_measure_2}]
+        )
         self.assertEqual(
             context['interesting'], [non_ordinal_measure_1])
         self.assertEqual(
@@ -626,7 +626,8 @@ class TestContextForOrgEmail(unittest.TestCase):
         self.assertEqual(
             context['worst'], [ordinal_measure_1])
         self.assertEqual(
-            context['most_changing']['improvements'], [(ordinal_measure_1,)])
+            context['most_changing']['improvements'],
+            [{'measure': ordinal_measure_1}])
 
 
 class TruncateSubjectTestCase(unittest.TestCase):

commit 5236c26bbc317b26c6b83b3140a71cc95979c649

Hide Analyse alert signup form until search is made
This was the original behaviour of the form but it was broken in
7979fc495. Previously if someone did try to subscribe without having
created a search it would create a SearchBookmark with an empty URL.
Since #2271 it now triggers a `MultipleObjectsReturned` error, which is
how it got spotted.

Closes #2062

openprescribing/media/js/src/analyse-chart.js
@@ -231,9 +231,9 @@ var analyseChart = {
     var alertForm = $(_this.el.alertForm);
     var title = encodeURIComponent(
       _this.globalOptions.friendly.chartTitle.replace(/<br\/>/g, ''));
-    alertForm.parent().show();
-    $('[name="url"]').val(this.hash);
-    $('[name="name"]').val(title);
+    alertForm.find('[name="url"]').val(this.hash);
+    alertForm.find('[name="name"]').val(title);
+    alertForm.show();
   },
 
   setUpSaveUrlUI: function() {

openprescribing/templates/_alert_signup_form.html
@@ -1,5 +1,5 @@
 {% load crispy_forms_tags %}
-<form method="post" class="form form-inline">
+<form method="post" class="form form-inline" id="alert-form" {% if alert_type == 'analyse' %}style="display: none"{% endif%}>
  {% csrf_token %}
  <p>
    {% if alert_type == 'analyse' %}