0beb6c01e2bfaa6ea16244b59d84eda192b02423
Tom Ward <tomward@fmail.co.uk>
2020-12-15 10:27:10 +0000

    Remove New Relic
    * no longer required

--- deploy/notify_deploy.py
+++ deploy/notify_deploy.py
@@ -5,22 +5,6 @@ import sys
 import dotenv
 
 
-# Newrelic Apps
-NEWRELIC_APPIDS = {"production": "45170403", "staging": "45937313", "test": "45170011"}
-
-
-def notify_newrelic(revision, url, fab_env="production"):
-    payload = {"deployment": {"revision": revision, "changelog": url}}
-    app_id = NEWRELIC_APPIDS[fab_env]
-    headers = {"X-Api-Key": os.environ["NEWRELIC_API_KEY"]}
-    response = requests.post(
-        ("https://api.newrelic.com/v2/applications/" "%s/deployments.json" % app_id),
-        headers=headers,
-        json=payload,
-    )
-    response.raise_for_status()
-
-
 def notify_slack(message):
     """Posts the message to #general"""
     # Set the webhook_url to the one provided by Slack when you create
@@ -44,5 +28,4 @@ if __name__ == "__main__":
     dotenv.read_dotenv(env_path, override=True)
 
     _, revision, url, fab_env = sys.argv
-    notify_newrelic(revision, url, fab_env=fab_env)
     notify_slack("A #deploy just finished. Changes here: {}".format(url))
