d72df78e0dea27ae93bde52e43cec360a963b32c
David Evans <dave.evans@thedatalab.org>
2021-11-24 14:01:48 +0000

    Delete measures from BigQuery as well

--- openprescribing/frontend/management/commands/delete_measure.py
+++ openprescribing/frontend/management/commands/delete_measure.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.core.management import BaseCommand, CommandError
 
 from frontend.models import Measure
+from gcutils.bigquery import Client
 
 
 class Command(BaseCommand):
@@ -15,6 +16,7 @@ class Command(BaseCommand):
             measure = Measure.objects.get(id=measure_id)
         except Measure.DoesNotExist:
             raise CommandError(f"No measure with ID '{measure_id}'")
+        delete_from_bigquery(measure_id)
         # The ON DELETE CASCADE configuration ensures that all MeasureValues are deleted
         # as well
         measure.delete()
@@ -22,3 +24,16 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("measure_id")
+
+
+def delete_from_bigquery(measure_id):
+    # Dataset name from `import_measures.MeasureCalculation.get_table()`
+    client = Client("measures")
+    # Table naming convention from `import_measures.MeasureCalculation.table_name()`
+    table_suffix = f"_data_{measure_id}"
+
+    tables_to_delete = [
+        table for table in client.list_tables() if table.table_id.endswith(table_suffix)
+    ]
+    for table in tables_to_delete:
+        client.delete_table(table.table_id)
