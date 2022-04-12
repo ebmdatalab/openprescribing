77b551a661d57d0d2b9dfd98f40633682eab9f44
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-04-12 12:25:39 +0100

    Convert config to es6

--- openprescribing/media/js/src/config.js
+++ openprescribing/media/js/src/config.js
@@ -1,10 +1,13 @@
 function getConfig() {
-  var config = {};
+  const config = {};
+
   if (typeof process.env.API_HOST === 'undefined') {
     config.apiHost = '';
   } else {
     config.apiHost = process.env.API_HOST;
   }
+
   return config;
 }
-module.exports = getConfig();
+
+export default getConfig();
