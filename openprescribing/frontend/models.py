f97ddcaae7d23582f1b437d9f6850c34a1333090
David Evans <dave.evans@thedatalab.org>
2022-12-01 17:35:24 +0000

    Change `JSONField` import
    The resulting migration is a no-op in database terms.

--- openprescribing/frontend/models.py
+++ openprescribing/frontend/models.py
@@ -3,7 +3,7 @@ import json
 import uuid
 
 from django.contrib.gis.db import models
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 from django.urls import reverse
