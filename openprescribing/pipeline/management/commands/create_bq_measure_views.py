cdea799319ae2390a3d4616930cacd887a9d1389
Peter Inglesby <peter.inglesby@gmail.com>
2024-01-18 15:17:16 +0000

    Don't hardcode list of measure views

--- openprescribing/pipeline/management/commands/create_bq_measure_views.py
+++ openprescribing/pipeline/management/commands/create_bq_measure_views.py
@@ -1,5 +1,5 @@
 import logging
-import os
+from pathlib import Path
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -43,41 +43,32 @@ def create_prescribing_tables():
     except Conflict:
         pass
 
+    base_path = Path().joinpath(
+        settings.APPS_ROOT, "frontend", "management", "commands", "measure_sql"
+    )
     for table_name in [
         "all_prescribing",
         "normalised_prescribing",
         "normalised_prescribing_standard",
         "raw_prescribing_normalised",
     ]:
-        recreate_table(client, table_name)
+        recreate_table(client, base_path / f"{table_name}.sql")
 
 
 def create_measure_tables():
     """Create tables that contain views on prescribing data for specific measures"""
 
     client = Client("measures")
-
-    for table_name in [
-        "dmd_objs_with_form_route",
-        "dmd_objs_hospital_only",
-        "practice_data_all_low_priority",
-        "vw__median_price_per_unit",
-        "vw__opioids_total_dmd",
-    ]:
-        recreate_table(client, table_name)
+    for path in (Path(settings.APPS_ROOT) / "measures" / "views").glob("vw__*.sql"):
+        recreate_table(client, path)
 
 
-def recreate_table(client, table_name):
+def recreate_table(client, path):
     """Create or recreate a table based on SQL in file with given name"""
 
+    table_name = path.stem
     logger.info("recreate_table: %s", table_name)
-    base_path = os.path.join(
-        settings.APPS_ROOT, "frontend", "management", "commands", "measure_sql"
-    )
-
-    path = os.path.join(base_path, table_name + ".sql")
-    with open(path, "r") as sql_file:
-        sql = sql_file.read()
+    sql = path.read_text()
 
     try:
         client.create_table_with_view(table_name, sql, False)
